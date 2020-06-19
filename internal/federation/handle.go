94d78120293e8a1a1bb1677cfffa8df1084cbac3
Kegan Dougal <kegan@matrix.org>
2020-06-19 17:46:04 +0100

    Finish impl of TestOutboundFederationSend

---
+++ internal/federation/handle.go
@@ -0,0 +1,192 @@
+package federation
+
+import (
+	"crypto/ed25519"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/matrix-org/gomatrixserverlib"
+)
+
+// HandleMakeSendJoinRequests is an option which will process make_join and send_join requests for rooms which are present
+// in this server. To add a room to this server, see Server.MustMakeRoom. No checks are done to see whether join requests
+// are allowed or not. If you wish to test that, write your own test.
+func HandleMakeSendJoinRequests() func(*Server) {
+	return func(s *Server) {
+		s.mux.Handle("/_matrix/federation/v1/make_join/{roomID}/{userID}", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			// Check federation signature
+			fedReq, errResp := gomatrixserverlib.VerifyHTTPRequest(
+				req, time.Now(), gomatrixserverlib.ServerName(s.serverName), s.keyRing,
+			)
+			if fedReq == nil {
+				w.WriteHeader(errResp.Code)
+				b, _ := json.Marshal(errResp.JSON)
+				w.Write(b)
+				return
+			}
+
+			vars := mux.Vars(req)
+			userID := vars["userID"]
+			roomID := vars["roomID"]
+
+			room, ok := s.rooms[roomID]
+			if !ok {
+				w.WriteHeader(404)
+				w.Write([]byte("complement: HandleMakeSendJoinRequests make_join unexpected room ID: " + roomID))
+				return
+			}
+
+			// Generate a join event
+			builder := gomatrixserverlib.EventBuilder{
+				Sender:     userID,
+				RoomID:     roomID,
+				Type:       "m.room.member",
+				StateKey:   &userID,
+				PrevEvents: []string{room.Timeline[len(room.Timeline)-1].EventID()},
+			}
+			err := builder.SetContent(map[string]interface{}{"membership": gomatrixserverlib.Join})
+			if err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte("complement: HandleMakeSendJoinRequests make_join cannot set membership content: " + err.Error()))
+				return
+			}
+			stateNeeded, err := gomatrixserverlib.StateNeededForEventBuilder(&builder)
+			if err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte("complement: HandleMakeSendJoinRequests make_join cannot calculate auth_events: " + err.Error()))
+				return
+			}
+			builder.AuthEvents = room.AuthEvents(stateNeeded)
+
+			// Send it
+			res := map[string]interface{}{
+				"event":        builder,
+				"room_version": room.Version,
+			}
+			w.WriteHeader(200)
+			b, _ := json.Marshal(res)
+			w.Write(b)
+		})).Methods("GET")
+
+		s.mux.Handle("/_matrix/federation/v2/send_join/{roomID}/{eventID}", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			fedReq, errResp := gomatrixserverlib.VerifyHTTPRequest(
+				req, time.Now(), gomatrixserverlib.ServerName(s.serverName), s.keyRing,
+			)
+			if fedReq == nil {
+				w.WriteHeader(errResp.Code)
+				b, _ := json.Marshal(errResp.JSON)
+				w.Write(b)
+				return
+			}
+			vars := mux.Vars(req)
+			roomID := vars["roomID"]
+
+			room, ok := s.rooms[roomID]
+			if !ok {
+				w.WriteHeader(404)
+				w.Write([]byte("complement: HandleMakeSendJoinRequests send_join unexpected room ID: " + roomID))
+				return
+			}
+			event, err := gomatrixserverlib.NewEventFromUntrustedJSON(fedReq.Content(), room.Version)
+			if err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte("complement: HandleMakeSendJoinRequests send_join cannot parse event JSON: " + err.Error()))
+			}
+			// insert the join event into the room state
+			room.AddEvent(&event)
+
+			// return current state and auth chain
+			b, err := json.Marshal(gomatrixserverlib.RespSendJoin{
+				AuthEvents:  room.AuthChain(),
+				StateEvents: room.AllCurrentState(),
+				Origin:      gomatrixserverlib.ServerName(s.serverName),
+			})
+			if err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte("complement: HandleMakeSendJoinRequests send_join cannot marshal RespSendJoin: " + err.Error()))
+			}
+			w.WriteHeader(200)
+			w.Write(b)
+		})).Methods("PUT")
+	}
+}
+
+// HandleDirectoryLookups will automatically return room IDs for any alias set via Server.Alias
+func HandleDirectoryLookups() func(*Server) {
+	return func(s *Server) {
+		s.mux.Handle("/_matrix/federation/v1/query/directory", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			alias := req.URL.Query().Get("room_alias")
+			if roomID, ok := s.aliases[alias]; ok {
+				b, err := json.Marshal(gomatrixserverlib.RespDirectory{
+					RoomID: roomID,
+					Servers: []gomatrixserverlib.ServerName{
+						gomatrixserverlib.ServerName(s.serverName),
+					},
+				})
+				if err != nil {
+					w.WriteHeader(500)
+					w.Write([]byte("complement: HandleDirectoryLookups failed to marshal JSON: " + err.Error()))
+					return
+				}
+				w.WriteHeader(200)
+				w.Write(b)
+				return
+			}
+			w.WriteHeader(404)
+			w.Write([]byte(`{
+				"errcode": "M_NOT_FOUND",
+				"error": "Room alias not found."
+			}`))
+		})).Methods("GET")
+	}
+}
+
+// HandleKeyRequests is an option which will process GET /_matrix/key/v2/server requests universally when requested.
+func HandleKeyRequests() func(*Server) {
+	return func(srv *Server) {
+		keymux := srv.mux.PathPrefix("/_matrix/key/v2").Subrouter()
+
+		certData, err := ioutil.ReadFile(srv.certPath)
+		if err != nil {
+			panic("failed to read cert file: " + err.Error())
+		}
+
+		keyFn := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			k := gomatrixserverlib.ServerKeys{}
+			k.ServerName = gomatrixserverlib.ServerName(srv.serverName)
+			publicKey := srv.priv.Public().(ed25519.PublicKey)
+			k.VerifyKeys = map[gomatrixserverlib.KeyID]gomatrixserverlib.VerifyKey{
+				srv.keyID: {
+					Key: gomatrixserverlib.Base64Bytes(publicKey),
+				},
+			}
+			k.OldVerifyKeys = map[gomatrixserverlib.KeyID]gomatrixserverlib.OldVerifyKey{}
+			k.TLSFingerprints = fingerprintPEM(certData)
+			k.ValidUntilTS = gomatrixserverlib.AsTimestamp(time.Now().Add(24 * time.Hour))
+			toSign, err := json.Marshal(k.ServerKeyFields)
+			if err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte("complement: HandleKeyRequests cannot marshal serverkeyfields: " + err.Error()))
+				return
+			}
+
+			k.Raw, err = gomatrixserverlib.SignJSON(
+				string(srv.serverName), srv.keyID, srv.priv, toSign,
+			)
+			if err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte("complement: HandleKeyRequests cannot sign json: " + err.Error()))
+				return
+			}
+			w.WriteHeader(200)
+			w.Write(k.Raw)
+		})
+
+		keymux.Handle("/server", keyFn).Methods("GET")
+		keymux.Handle("/server/", keyFn).Methods("GET")
+		keymux.Handle("/server/{keyID}", keyFn).Methods("GET")
+	}
+}
--- tests/federation_send_test.go
+++ tests/federation_send_test.go
@@ -1,6 +1,9 @@
 package tests
 
 import (
+	"encoding/json"
+	"net/http"
+	"sync"
 	"testing"
 
 	"github.com/matrix-org/complement/internal/b"
@@ -32,4 +35,38 @@ func TestOutboundFederationSend(t *testing.T) {
 	alice := deployment.Client(t, "hs1", "@alice:hs1")
 	alice.MustDo(t, "POST", []string{"_matrix", "client", "r0", "join", roomAlias}, struct{}{})
 
+	wantEventType := "m.room.message"
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	srv.Mux().Handle("/_matrix/federation/v1/send/{txnID}", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		defer wg.Done()
+		var body gomatrixserverlib.Transaction
+		if err := json.NewDecoder(req.Body).Decode(&body); err != nil {
+			t.Errorf("failed to decode /send body: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		if len(body.PDUs) != 1 {
+			t.Fatalf("got %d pdus, want %d", len(body.PDUs), 1)
+		}
+		ev, err := gomatrixserverlib.NewEventFromUntrustedJSON(body.PDUs[0], ver)
+		if err != nil {
+			t.Fatalf("PDU failed NewEventFromUntrustedJSON checks: %s", err)
+		}
+		if ev.Type() != wantEventType {
+			t.Errorf("Wrong event type, got %s want %s", ev.Type(), wantEventType)
+		}
+		w.WriteHeader(200)
+	})).Methods("PUT")
+
+	alice.MustDo(t, "PUT", []string{"_matrix", "client", "r0", "rooms", serverRoom.RoomID, "send", wantEventType, "1"}, struct {
+		Msgtype string `json:"msgtype"`
+		Body    string `json:"body"`
+	}{
+		Msgtype: "m.text",
+		Body:    "Hello world!",
+	})
+	t.Logf("alice sent message")
+	wg.Wait()
 }
