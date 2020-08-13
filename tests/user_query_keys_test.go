18bf45b4f63a52538a96e817ceeeff03d457da1c
Kegan Dougal <kegan@matrix.org>
2020-08-13 14:31:26 +0100

    Prose

--- tests/user_query_keys_test.go
+++ tests/user_query_keys_test.go
@@ -8,7 +8,12 @@ import (
 	"github.com/matrix-org/complement/internal/must"
 )
 
-func TestKeysQueryWithDeviceIDAsStringFails(t *testing.T) {
+// Endpoint: https://matrix.org/docs/spec/client_server/r0.6.1#post-matrix-client-r0-keys-query
+// This test asserts that the server is correctly rejecting input that does not match the request format given.
+// Specifically, it replaces [$device_id] with { $device_id: bool } which, if not type checked, will be processed
+// like an array in Python and hence go un-noticed. In Go however it will result in a 400. The correct behaviour is
+// to return a 400. Element iOS uses this erroneous format.
+func TestKeysQueryWithDeviceIDAsObjectFails(t *testing.T) {
 	deployment := Deploy(t, "user_query_keys", b.BlueprintAlice)
 	defer deployment.Destroy(t)
 
