3077f14fd86005d7e916040da5bb2463389d80a0
Kegan Dougal <kegan@matrix.org>
2020-12-01 16:50:06 +0000

    Add MSC2836 tests, run with go test --tags=msc2836

--- tests/msc2836_test.go
+++ tests/msc2836_test.go
@@ -1,11 +1,16 @@
-// +buaaild msc2836
+// +build msc2836
 
 package tests
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"net/http"
+	"sort"
+	"strings"
 	"testing"
 	"time"
 
@@ -16,6 +21,148 @@ import (
 	"github.com/matrix-org/gomatrixserverlib"
 	"github.com/tidwall/gjson"
 )
+
+// This test checks that federated threading works when the remote server joins after the messages
+// have been sent. The test configures a thread like:
+//    A
+//    |
+//    B
+//   / \
+//  C   D
+// Then a remote server joins the room. /event_relationships is then hit with event ID 'D' which the
+// joined server does not have. This should cause a remote /event_relationships request to service the
+// request. The request parameters will pull in events D,B. This gets repeated for a second time with
+// an event which the server does have, event B, to ensure that this request also works and also does
+// federated hits to return missing events (A,C).
+func TestEventRelationships(t *testing.T) {
+	deployment := Deploy(t, "msc2836", b.BlueprintFederationOneToOneRoom)
+	defer deployment.Destroy(t)
+
+	// Create the room and send events A,B,C,D
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+	roomID := alice.CreateRoom(t, map[string]interface{}{
+		"preset": "public_chat",
+	})
+	eventA := alice.SendEventSynced(t, roomID, b.Event{
+		Type: "m.room.message",
+		Content: map[string]interface{}{
+			"msgtype": "m.text",
+			"body":    "Message A",
+		},
+	})
+	eventB := alice.SendEventSynced(t, roomID, b.Event{
+		Type: "m.room.message",
+		Content: map[string]interface{}{
+			"msgtype": "m.text",
+			"body":    "Message B",
+			"m.relationship": map[string]interface{}{
+				"rel_type": "m.reference",
+				"event_id": eventA,
+			},
+		},
+	})
+	eventC := alice.SendEventSynced(t, roomID, b.Event{
+		Type: "m.room.message",
+		Content: map[string]interface{}{
+			"msgtype": "m.text",
+			"body":    "Message C",
+			"m.relationship": map[string]interface{}{
+				"rel_type": "m.reference",
+				"event_id": eventB,
+			},
+		},
+	})
+	eventD := alice.SendEventSynced(t, roomID, b.Event{
+		Type: "m.room.message",
+		Content: map[string]interface{}{
+			"msgtype": "m.text",
+			"body":    "Message D",
+			"m.relationship": map[string]interface{}{
+				"rel_type": "m.reference",
+				"event_id": eventB,
+			},
+		},
+	})
+	t.Logf("Event ID A:%s  B:%s  C:%s  D:%s", eventA, eventB, eventC, eventD)
+
+	// Join the room from another server
+	bob := deployment.Client(t, "hs2", "@bob:hs2")
+	_ = bob.JoinRoom(t, roomID, []string{"hs1"})
+
+	// Now hit /event_relationships with eventD
+	res := bob.MustDo(t, "POST", []string{"_matrix", "client", "unstable", "event_relationships"}, map[string]interface{}{
+		"event_id":       eventD,
+		"room_id":        roomID, // required so the server knows which servers to ask
+		"direction":      "down", // no newer events, so nothing should be added
+		"include_parent": true,   // this should pull in event B
+	})
+	var gots []gjson.Result
+	must.MatchResponse(t, res, match.HTTPResponse{
+		JSON: []match.JSON{
+			match.JSONKeyEqual("limited", false),
+			match.JSONArrayEach("events", func(r gjson.Result) error {
+				gots = append(gots, r)
+				return nil
+			}),
+		},
+	})
+	if len(gots) != 2 {
+		t.Fatalf("/event_relationships got %d events, want 2", len(gots))
+	}
+	if gots[0].Get("event_id").Str != eventD {
+		t.Fatalf("/event_relationships expected first element to be event D but was %s", gots[0].Raw)
+	}
+	if gots[1].Get("event_id").Str != eventB {
+		t.Fatalf("/event_relationships expected second element to be event B but was %s", gots[1].Raw)
+	}
+	// check the children count of event B to make sure it is 2 (C,D)
+	// and check the hash is correct
+	checkUnsigned(t, gots[1], map[string]int64{
+		"m.reference": 2,
+	}, []string{eventC, eventD})
+
+	// now hit /event_relationships again with B, which should return everything (and fetch the missing events A,C)
+	res = bob.MustDo(t, "POST", []string{"_matrix", "client", "unstable", "event_relationships"}, map[string]interface{}{
+		"event_id":       eventB,
+		"room_id":        roomID, // required so the server knows which servers to ask
+		"direction":      "down", // this pulls in C,D
+		"include_parent": true,   // this pulls in A
+		"recent_first":   false,
+	})
+	gots = []gjson.Result{}
+	must.MatchResponse(t, res, match.HTTPResponse{
+		JSON: []match.JSON{
+			match.JSONKeyEqual("limited", false),
+			match.JSONArrayEach("events", func(r gjson.Result) error {
+				gots = append(gots, r)
+				return nil
+			}),
+		},
+	})
+	if len(gots) != 4 {
+		t.Fatalf("/event_relationships returned %d events, want 4. %v", len(gots), gots)
+	}
+	if gots[0].Get("event_id").Str != eventB {
+		t.Fatalf("/event_relationships expected first element to be event B but was %s", gots[0].Raw)
+	}
+	if gots[1].Get("event_id").Str != eventA {
+		t.Fatalf("/event_relationships expected second element to be event A but was %s", gots[1].Raw)
+	}
+	if gots[2].Get("event_id").Str != eventC {
+		t.Fatalf("/event_relationships expected third element to be event C but was %s", gots[2].Raw)
+	}
+	if gots[3].Get("event_id").Str != eventD {
+		t.Fatalf("/event_relationships expected fourth element to be event D but was %s", gots[3].Raw)
+	}
+	// event A has event B as a child
+	checkUnsigned(t, gots[1], map[string]int64{
+		"m.reference": 1,
+	}, []string{eventB})
+	// event B has events C,D as children (same as before)
+	checkUnsigned(t, gots[0], map[string]int64{
+		"m.reference": 2,
+	}, []string{eventC, eventD})
+}
 
 // This test checks that the homeserver makes a federated request to /event_relationships
 // when walking a thread when it encounters an unknown event ID. The test configures a
@@ -146,7 +293,7 @@ func TestFederatedEventRelationships(t *testing.T) {
 	// join the room on HS1
 	// HS1 will not have any of these messages, only the room state.
 	alice := deployment.Client(t, "hs1", "@alice:hs1")
-	alice.JoinRoom(t, room.RoomID)
+	alice.JoinRoom(t, room.RoomID, []string{srv.ServerName})
 
 	// send a new child in the thread (child of D) so the HS has something to latch on to.
 	eventE := srv.MustCreateEvent(t, room, b.Event{
@@ -214,4 +361,26 @@ func TestFederatedEventRelationships(t *testing.T) {
 		},
 	})
 	must.HaveInOrder(t, gotEventIDs, []string{eventA.EventID(), eventB.EventID(), eventC.EventID()})
+}
+
+func checkUnsigned(t *testing.T, r gjson.Result, wantChildCount map[string]int64, childrenEventIDs []string) {
+	t.Helper()
+	childCountMap := r.Get(`unsigned.children`)
+	if !childCountMap.IsObject() {
+		t.Fatalf("checkUnsigned: unsigned.children isn't a map")
+	}
+	childCount := childCountMap.Map()
+	for wantRelType, wantCount := range wantChildCount {
+		if childCount[wantRelType].Int() != wantCount {
+			t.Errorf("rel_type %s got %d count want %d", wantRelType, childCount[wantRelType].Int(), wantCount)
+		}
+	}
+	// check the hash is correct
+	sort.Strings(childrenEventIDs)
+	wantHash := sha256.Sum256([]byte(strings.Join(childrenEventIDs, "")))
+	gotHash, err := base64.RawStdEncoding.DecodeString(r.Get(`unsigned.children_hash`).Str)
+	must.NotError(t, "failed to decode hash as base64", err)
+	if !bytes.Equal(gotHash, wantHash[:]) {
+		t.Errorf("mismatched children_hash: got %x want %x", gotHash, wantHash[:])
+	}
 }
