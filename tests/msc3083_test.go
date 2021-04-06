aea126c311b5dd45d6c89fc8b0bdde16a47f6ea5
Patrick Cloke <patrickc@matrix.org>
2021-04-06 10:25:22 -0400

    Add tests for joining over federation.

--- tests/msc3083_test.go
+++ tests/msc3083_test.go
@@ -18,7 +18,7 @@ var (
 	spaceParentEventType = "org.matrix.msc1772.space.parent"
 )
 
-func FailJoinRoom(c *client.CSAPI, t *testing.T, roomIDOrAlias string, serverName string) {
+func FailJoinRoom(c *client.CSAPI, t *testing.T, roomIDOrAlias string, serverName string, expectedHttpCode int) {
 	// This is copied from Client.JoinRoom to test a join failure.
 	query := make(url.Values, 1)
 	query.Set("server_name", serverName)
@@ -29,13 +29,13 @@ func FailJoinRoom(c *client.CSAPI, t *testing.T, roomIDOrAlias string, serverNam
 		nil,
 		"application/json",
 		query,
-		403,
+		expectedHttpCode,
 	)
 }
 
 // Test joining a room with join rules restricted to membership in a space.
 func TestRestrictedRoomsLocalJoin(t *testing.T) {
-	deployment := Deploy(t, "msc3083", b.BlueprintOneToOneRoom)
+	deployment := Deploy(t, "msc3083_local", b.BlueprintOneToOneRoom)
 	defer deployment.Destroy(t)
 
 	// Create the space and put a room in it.
@@ -75,7 +75,7 @@ func TestRestrictedRoomsLocalJoin(t *testing.T) {
 
 	// Create a second user and attempt to join the room, it should fail.
 	bob := deployment.Client(t, "hs1", "@bob:hs1")
-	FailJoinRoom(bob, t, room, "hs1")
+	FailJoinRoom(bob, t, room, "hs1", 403)
 
 	// Join the space, attempt to join the room again, which now should succeed.
 	bob.JoinRoom(t, space, []string{"hs1"})
@@ -84,7 +84,7 @@ func TestRestrictedRoomsLocalJoin(t *testing.T) {
 	// Leaving the room works and the user is unable to re-join.
 	bob.LeaveRoom(t, room)
 	bob.LeaveRoom(t, space)
-	FailJoinRoom(bob, t, room, "hs1")
+	FailJoinRoom(bob, t, room, "hs1", 403)
 
 	// Invite the user and joining should work.
 	alice.InviteRoom(t, room, "@bob:hs1")
@@ -111,7 +111,7 @@ func TestRestrictedRoomsLocalJoin(t *testing.T) {
 		},
 	)
 	// Fails since invalid values get filtered out of allow.
-	FailJoinRoom(bob, t, room, "hs1")
+	FailJoinRoom(bob, t, room, "hs1", 403)
 
 	alice.SendEventSynced(
 		t,
@@ -127,5 +127,55 @@ func TestRestrictedRoomsLocalJoin(t *testing.T) {
 		},
 	)
 	// Fails since a fully invalid allow key rquires an invite.
-	FailJoinRoom(bob, t, room, "hs1")
+	FailJoinRoom(bob, t, room, "hs1", 403)
+}
+
+// Test joining a room with join rules restricted to membership in a space.
+func TestRestrictedRoomsRemoteJoin(t *testing.T) {
+	deployment := Deploy(t, "msc3083_remote", b.BlueprintFederationOneToOneRoom)
+	defer deployment.Destroy(t)
+
+	// Create the space and put a room in it.
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+	space := alice.CreateRoom(t, map[string]interface{}{
+		"preset": "public_chat",
+		"name":   "Space",
+	})
+	// The room is an unstable room version which supports the restricted join_rule.
+	room := alice.CreateRoom(t, map[string]interface{}{
+		"preset":       "public_chat",
+		"name":         "Room",
+		"room_version": "org.matrix.msc3083",
+		"initial_state": []map[string]interface{}{
+			{
+				"type":      "m.room.join_rules",
+				"state_key": "",
+				"content": map[string]interface{}{
+					"join_rule": "restricted",
+					"allow": []map[string]interface{}{
+						{
+							"space": &space,
+							"via":   []string{"hs1"},
+						},
+					},
+				},
+			},
+		},
+	})
+	alice.SendEventSynced(t, space, b.Event{
+		Type:     spaceChildEventType,
+		StateKey: &room,
+		Content: map[string]interface{}{
+			"via": []string{"hs1"},
+		},
+	})
+
+	// Create a second user and attempt to join the room, it should fail.
+	bob := deployment.Client(t, "hs2", "@bob:hs2")
+	// Note that this is a 400 error because it comes back over federation.
+	FailJoinRoom(bob, t, room, "hs1", 400)
+
+	// Join the space, attempt to join the room again, which now should succeed.
+	bob.JoinRoom(t, space, []string{"hs1"})
+	bob.JoinRoom(t, room, []string{"hs1"})
 }
