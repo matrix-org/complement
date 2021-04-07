8863736f51b498e0c27e25cbcebb672567937b70
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-04-07 17:35:53 +0530

    tests: add GET /login test (#97)
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_login_test.go
+++ tests/apidoc_login_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/tidwall/gjson"
@@ -16,6 +17,27 @@ func TestLogin(t *testing.T) {
 	defer deployment.Destroy(t)
 	unauthedClient := deployment.Client(t, "hs1", "")
 	t.Run("parallel", func(t *testing.T) {
+		// sytest: GET /login yields a set of flows
+		t.Run("GET /login yields a set of flows", func(t *testing.T) {
+			t.Parallel()
+			res := unauthedClient.MustDo(t, "GET", []string{"_matrix", "client", "r0", "login"}, json.RawMessage(`{}`))
+			must.MatchResponse(t, res, match.HTTPResponse{
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+				},
+				JSON: []match.JSON{
+					// TODO(paul): Spec is a little vague here. Spec says that every
+					//   option needs a 'stages' key, but the implementation omits it
+					//   for options that have only one stage in their flow.
+					match.JSONArrayEach("flows", func(val gjson.Result) error {
+						if !(val.Get("stages").IsArray()) && (val.Get("type").Raw == "") {
+							return fmt.Errorf("expected flow to have a list of 'stages' or a 'type' : %v %v", val.Get("type").Raw, val.Get("stages"))
+						}
+						return nil
+					}),
+				},
+			})
+		})
 		// sytest: POST /login can log in as a user
 		t.Run("POST /login can login as user", func(t *testing.T) {
 			t.Parallel()
@@ -32,8 +54,6 @@ func TestLogin(t *testing.T) {
 					match.JSONKeyTypeEqual("user_id", gjson.String),
 				},
 			})
-			//body, _ := ioutil.ReadAll(res.Body)
-			//userId := gjson.Get(string(body), "user_id")
 			res = unauthedClient.MustDo(t, "POST", []string{"_matrix", "client", "r0", "login"}, json.RawMessage(`{
 				"type": "m.login.password",
 				"identifier": {
