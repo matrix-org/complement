db3f7c374cb2c65560f0687d5993b48c83a1a5c7
Meenal Trivedi <meenaltrivedi6102@gmail.com>
2021-04-08 03:15:31 +0530

    fix
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_request_encoding_test.go
+++ tests/apidoc_request_encoding_test.go
@@ -14,7 +14,7 @@ func TestRequestEncodingFails(t *testing.T) {
 	deployment := Deploy(t, "request_encoding", b.BlueprintAlice)
 	defer deployment.Destroy(t)
 	unauthedClient := deployment.Client(t, "hs1", "")
-	testString := `{ "test": "a' . \x81 . '" }`
+	testString := `{ "test": "a' . ` + "\x81" + ` . '" }`
 	// sytest: POST rejects invalid utf-8 in JSON
 	t.Run("POST rejects invalid utf-8 in JSON", func(t *testing.T) {
 		res := unauthedClient.MustDo(t, "POST", []string{"_matrix", "client", "r0", "register"}, json.RawMessage(testString))
