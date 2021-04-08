5425d5d2b51224744d16d2c974b0716689835ca4
Patrick Cloke <patrickc@matrix.org>
2021-04-08 10:00:37 -0400

    Rename variable to avoid conflicting with msc2946_test.

--- tests/msc3083_test.go
+++ tests/msc3083_test.go
@@ -14,8 +14,7 @@ import (
 )
 
 var (
-	spaceChildEventType  = "org.matrix.msc1772.space.child"
-	spaceParentEventType = "org.matrix.msc1772.space.parent"
+	msc1772SpaceChildEventType  = "org.matrix.msc1772.space.child"
 )
 
 func FailJoinRoom(c *client.CSAPI, t *testing.T, roomIDOrAlias string, serverName string, expectedHttpCode int) {
@@ -66,7 +65,7 @@ func TestRestrictedRoomsLocalJoin(t *testing.T) {
 		},
 	})
 	alice.SendEventSynced(t, space, b.Event{
-		Type:     spaceChildEventType,
+		Type:     msc1772SpaceChildEventType,
 		StateKey: &room,
 		Content: map[string]interface{}{
 			"via": []string{"hs1"},
@@ -163,7 +162,7 @@ func TestRestrictedRoomsRemoteJoin(t *testing.T) {
 		},
 	})
 	alice.SendEventSynced(t, space, b.Event{
-		Type:     spaceChildEventType,
+		Type:     msc1772SpaceChildEventType,
 		StateKey: &room,
 		Content: map[string]interface{}{
 			"via": []string{"hs1"},
