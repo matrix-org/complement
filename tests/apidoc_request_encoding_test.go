5cbd2050cc08769e7ff11fa5494853f0f32f53f2
Meenal Trivedi <meenaltrivedi6102@gmail.com>
2021-04-09 19:14:21 +0530

    replace mustDo to Do
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_request_encoding_test.go
+++ tests/apidoc_request_encoding_test.go
@@ -14,10 +14,13 @@ func TestRequestEncodingFails(t *testing.T) {
 	deployment := Deploy(t, "request_encoding", b.BlueprintAlice)
 	defer deployment.Destroy(t)
 	unauthedClient := deployment.Client(t, "hs1", "")
-	testString := `{ "test": "a' . ` + "\x81" + ` . '" }`
+	testString := `{ "test":"a` + "\x81" + `" }`
 	// sytest: POST rejects invalid utf-8 in JSON
 	t.Run("POST rejects invalid utf-8 in JSON", func(t *testing.T) {
-		res := unauthedClient.MustDo(t, "POST", []string{"_matrix", "client", "r0", "register"}, json.RawMessage(testString))
+		res, err := unauthedClient.Do(t, "POST", []string{"_matrix", "client", "r0", "register"}, json.RawMessage(testString), nil)
+		if err != nil {
+			t.Fatalf("failed to make request to register %v", err)
+		}
 		must.MatchResponse(t, res, match.HTTPResponse{
 			StatusCode: 400,
 			JSON: []match.JSON{
