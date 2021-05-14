288b9227d45c9a124b5ea7de26af17ca2d58c94b
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-05-14 15:03:36 +0530

    tests: add user_presence tests (#104)
    * tests: add user_presence tests
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    * Update apidoc_presence_test.go
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix: client
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

---
+++ tests/apidoc_presence_test.go
@@ -0,0 +1,30 @@
+// +build !dendrite_blacklist
+
+// Rationale for being included in Dendrite's blacklist: https://github.com/matrix-org/complement/pull/104#discussion_r617646624
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/matrix-org/complement/internal/b"
+	"github.com/matrix-org/complement/internal/match"
+	"github.com/matrix-org/complement/internal/must"
+)
+
+func TestPresence(t *testing.T) {
+	deployment := Deploy(t, "presence", b.BlueprintAlice)
+	defer deployment.Destroy(t)
+
+	authedClient := deployment.Client(t, "hs1", "@alice:hs1")
+	// sytest: GET /presence/:user_id/status fetches initial status
+	t.Run("GET /presence/:user_id/status fetches initial status", func(t *testing.T) {
+		res := authedClient.MustDo(t, "GET", []string{"_matrix", "client", "r0", "presence", "@alice:hs1", "status"}, nil)
+
+		must.MatchResponse(t, res, match.HTTPResponse{
+			JSON: []match.JSON{
+				match.JSONKeyPresent("presence"),
+			},
+		})
+	})
+}
