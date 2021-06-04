490b6aa35bdd3800103cce1fb84d73665682a1aa
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-06-04 22:25:54 +0530

    feat: Implement remaining presence test (#121)
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_presence_test.go
+++ tests/apidoc_presence_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/matrix-org/complement/internal/b"
+	"github.com/matrix-org/complement/internal/client"
 	"github.com/matrix-org/complement/internal/match"
 	"github.com/matrix-org/complement/internal/must"
 )
@@ -23,6 +24,25 @@ func TestPresence(t *testing.T) {
 		must.MatchResponse(t, res, match.HTTPResponse{
 			JSON: []match.JSON{
 				match.JSONKeyPresent("presence"),
+			},
+		})
+	})
+	// sytest: PUT /presence/:user_id/status updates my presence
+	t.Run("PUT /presence/:user_id/status updates my presence", func(t *testing.T) {
+		statusMsg := "Testing something"
+		reqBody := client.WithJSONBody(t, map[string]interface{}{
+			"status_msg": statusMsg,
+			"presence":   "online",
+		})
+		res := authedClient.DoFunc(t, "PUT", []string{"_matrix", "client", "r0", "presence", "@alice:hs1", "status"}, reqBody)
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+		})
+		res = authedClient.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "presence", "@alice:hs1", "status"})
+		must.MatchResponse(t, res, match.HTTPResponse{
+			JSON: []match.JSON{
+				match.JSONKeyPresent("presence"),
+				match.JSONKeyEqual("status_msg", statusMsg),
 			},
 		})
 	})
