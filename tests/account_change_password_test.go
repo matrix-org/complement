7daffed61a5432a90f02a9718063b1be8f23cf6c
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-06-08 17:17:55 +0530

    feat: add more change_password_tests (#120)
    * feat: add can_login_with_new_password test
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/account_change_password_test.go
+++ tests/account_change_password_test.go
@@ -12,39 +12,63 @@ import (
 func TestChangePassword(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
-	passwordClient := deployment.RegisterUser(t, "hs1", "cant_login_with_old_password", "superuser")
+	oldPassword := "superuser"
+	newPassword := "my_new_password"
+	passwordClient := deployment.RegisterUser(t, "hs1", "test_change_password_user", oldPassword)
 	unauthedClient := deployment.Client(t, "hs1", "")
-	newPassword := "new_superuser"
 	// sytest: After changing password, can't log in with old password
 	t.Run("After changing password, can't log in with old password", func(t *testing.T) {
+
+		changePassword(t, passwordClient, oldPassword, newPassword)
+
 		reqBody := client.WithJSONBody(t, map[string]interface{}{
-			"auth": map[string]interface{}{
-				"type":     "m.login.password",
-				"user":     passwordClient.UserID,
-				"password": "superuser",
-			},
-			"new_password": newPassword,
-		})
-		res := passwordClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "account", "password"}, reqBody)
-
-		must.MatchResponse(t, res, match.HTTPResponse{
-			StatusCode: 200,
-		})
-
-		reqBody = client.WithJSONBody(t, map[string]interface{}{
 			"identifier": map[string]interface{}{
 				"type": "m.id.user",
 				"user": passwordClient.UserID,
 			},
 			"type":     "m.login.password",
-			"password": "superuser",
+			"password": oldPassword,
 		})
-		res = unauthedClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "login"}, reqBody)
+		res := unauthedClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "login"}, reqBody)
 		must.MatchResponse(t, res, match.HTTPResponse{
 			StatusCode: 403,
 			JSON: []match.JSON{
 				match.JSONKeyEqual("errcode", "M_FORBIDDEN"),
 			},
 		})
+	})
+	// sytest: After changing password, can log in with new password
+	t.Run("After changing password, can log in with new password", func(t *testing.T) {
+
+		reqBody := client.WithJSONBody(t, map[string]interface{}{
+			"identifier": map[string]interface{}{
+				"type": "m.id.user",
+				"user": passwordClient.UserID,
+			},
+			"type":     "m.login.password",
+			"password": newPassword,
+		})
+		res := unauthedClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "login"}, reqBody)
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+		})
+	})
+}
+
+func changePassword(t *testing.T, passwordClient *client.CSAPI, oldPassword string, newPassword string) {
+	t.Helper()
+	reqBody := client.WithJSONBody(t, map[string]interface{}{
+		"auth": map[string]interface{}{
+			"type":     "m.login.password",
+			"user":     passwordClient.UserID,
+			"password": oldPassword,
+		},
+		"new_password": newPassword,
+	})
+
+	res := passwordClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "account", "password"}, reqBody)
+
+	must.MatchResponse(t, res, match.HTTPResponse{
+		StatusCode: 200,
 	})
 }
