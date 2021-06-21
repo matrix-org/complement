ab4304ae87c2730aa72e42e5347d06341923ce8d
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-06-21 16:06:07 +0530

    feat: session related change-password tests (#126)
    * feat: session related change-password tests
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * add optinally keep tests
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * requested changes
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * update sytesT_coverage
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/account_change_password_test.go
+++ tests/account_change_password_test.go
@@ -1,12 +1,16 @@
 package tests
 
 import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/matrix-org/complement/internal/b"
 	"github.com/matrix-org/complement/internal/client"
+	"github.com/matrix-org/complement/internal/docker"
 	"github.com/matrix-org/complement/internal/match"
 	"github.com/matrix-org/complement/internal/must"
+
+	"github.com/tidwall/gjson"
 )
 
 func TestChangePassword(t *testing.T) {
@@ -16,6 +20,7 @@ func TestChangePassword(t *testing.T) {
 	newPassword := "my_new_password"
 	passwordClient := deployment.RegisterUser(t, "hs1", "test_change_password_user", oldPassword)
 	unauthedClient := deployment.Client(t, "hs1", "")
+	sessionTest := createSession(t, deployment, "test_change_password_user", "superuser")
 	// sytest: After changing password, can't log in with old password
 	t.Run("After changing password, can't log in with old password", func(t *testing.T) {
 
@@ -51,6 +56,48 @@ func TestChangePassword(t *testing.T) {
 		res := unauthedClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "login"}, reqBody)
 		must.MatchResponse(t, res, match.HTTPResponse{
 			StatusCode: 200,
+			JSON: []match.JSON{
+				match.JSONKeyEqual("user_id", passwordClient.UserID),
+			},
+		})
+	})
+	// sytest: After changing password, existing session still works
+	t.Run("After changing password, existing session still works", func(t *testing.T) {
+		res := passwordClient.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "account", "whoami"})
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+		})
+	})
+	// sytest: After changing password, a different session no longer works by default
+	t.Run("After changing password, a different session no longer works by default", func(t *testing.T) {
+		res := sessionTest.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "account", "whoami"})
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 401,
+		})
+	})
+
+	// sytest: After changing password, different sessions can optionally be kept
+	t.Run("After changing password, different sessions can optionally be kept", func(t *testing.T) {
+		sessionOptional := createSession(t, deployment, "test_change_password_user", newPassword)
+		reqBody := client.WithJSONBody(t, map[string]interface{}{
+			"auth": map[string]interface{}{
+				"type":     "m.login.password",
+				"user":     passwordClient.UserID,
+				"password": newPassword,
+			},
+			"new_password":   "new_optional_password",
+			"logout_devices": false,
+		})
+
+		res := passwordClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "account", "password"}, reqBody)
+
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+		})
+		res = sessionOptional.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "account", "whoami"})
+
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
 		})
 	})
 }
@@ -71,4 +118,25 @@ func changePassword(t *testing.T, passwordClient *client.CSAPI, oldPassword stri
 	must.MatchResponse(t, res, match.HTTPResponse{
 		StatusCode: 200,
 	})
+}
+
+func createSession(t *testing.T, deployment *docker.Deployment, userID, password string) *client.CSAPI {
+	authedClient := deployment.Client(t, "hs1", "")
+	reqBody := client.WithJSONBody(t, map[string]interface{}{
+		"identifier": map[string]interface{}{
+			"type": "m.id.user",
+			"user": userID,
+		},
+		"type":     "m.login.password",
+		"password": password,
+	})
+	res := authedClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "login"}, reqBody)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("unable to read response body: %v", err)
+	}
+
+	authedClient.UserID = gjson.GetBytes(body, "user_id").Str
+	authedClient.AccessToken = gjson.GetBytes(body, "access_token").Str
+	return authedClient
 }
