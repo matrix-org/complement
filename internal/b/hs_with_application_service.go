c03e49e95f233ef3191829d35896b23550edc8cf
Eric Eastwood <contact@ericeastwood.com>
2021-06-21 16:35:27 -0500

    Fix blueprint name conflict
    The name conflict would result in the following error when some alice tests ran before the MSC2716 tests.
    ```
    === RUN   TestBackfillingHistory
        msc2716_test.go:54: Deploy times: 18.499364ms blueprints, 1.581230486s containers
        msc2716_test.go:60: Deployment.Client - HS name 'hs1' - user ID '@the-bridge-user:hs1' not found
    ```

--- internal/b/hs_with_application_service.go
+++ internal/b/hs_with_application_service.go
@@ -2,7 +2,7 @@ package b
 
 // BlueprintHSWithApplicationService who has an application service to interact with
 var BlueprintHSWithApplicationService = MustValidate(Blueprint{
-	Name: "alice",
+	Name: "hs_with_application_service",
 	Homeservers: []Homeserver{
 		{
 			Name: "hs1",
