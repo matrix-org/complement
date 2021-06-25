47575053aba66620e55b0952bc1e315c866c9d75
Meenal Trivedi <meenaltrivedi6102@gmail.com>
2021-06-26 00:36:55 +0530

    fix
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_profile_avatar_url_test.go
+++ tests/apidoc_profile_avatar_url_test.go
@@ -1,10 +1,10 @@
 package tests
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/matrix-org/complement/internal/b"
+	"github.com/matrix-org/complement/internal/client"
 	"github.com/matrix-org/complement/internal/match"
 	"github.com/matrix-org/complement/internal/must"
 )
@@ -13,13 +13,14 @@ func TestProfileAvatarURL(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
 	unauthedClient := deployment.Client(t, "hs1", "")
-	authedClient := deployment.RegisterUser(t, "hs1", "test_avatar_url_user", "superuser")
+	authedClient := deployment.Client(t, "hs1", "@alice:hs1")
 	avatarURL := "mxc://example.com/SEsfnsuifSDFSSEF"
 	// sytest: PUT /profile/:user_id/avatar_url sets my avatar
 	t.Run("PUT /profile/:user_id/avatar_url sets my avatar", func(t *testing.T) {
-		res := authedClient.MustDo(t, "PUT", []string{"_matrix", "client", "r0", "profile", authedClient.UserID, "avatar_url"}, json.RawMessage(`{
-			"avatar_url": "`+avatarURL+`"
-			}`))
+		reqBody := client.WithJSONBody(t, map[string]interface{}{
+			"avatar_url": avatarURL,
+		})
+		res := authedClient.MustDoFunc(t, "PUT", []string{"_matrix", "client", "r0", "profile", authedClient.UserID, "avatar_url"}, reqBody)
 
 		must.MatchResponse(t, res, match.HTTPResponse{
 			StatusCode: 200,
