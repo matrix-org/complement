13a0c186ff26ac0762c7487a119bd137be9742ee
Meenal Trivedi <meenaltrivedi6102@gmail.com>
2021-07-01 20:34:59 +0530

    fix
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_room_create_test.go
+++ tests/apidoc_room_create_test.go
@@ -1,24 +1,26 @@
 package tests
 
 import (
+	"testing"
+
+	"github.com/tidwall/gjson"
+
 	"github.com/matrix-org/complement/internal/b"
 	"github.com/matrix-org/complement/internal/client"
 	"github.com/matrix-org/complement/internal/match"
 	"github.com/matrix-org/complement/internal/must"
-	"github.com/tidwall/gjson"
-	"testing"
 )
 
-func TestRoomCreate(t *testing.T){
+func TestRoomCreate(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
 	authedClient := deployment.Client(t, "hs1", "@alice:hs1")
-	t.Run("Parallel", func(t *testing.T){
+	t.Run("Parallel", func(t *testing.T) {
 		// Sytest: POST /createRoom makes a public room
 		t.Run("POST /createRoom makes a public room", func(t *testing.T) {
 			t.Parallel()
 			reqBody := client.WithJSONBody(t, map[string]interface{}{
-				"visibility": "public",
+				"visibility":      "public",
 				"room_alias_name": "30-room-create-alias-random",
 			})
 			res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
