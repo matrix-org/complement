565306889bfad2d9a78c57c1f0a3aacd3d3984e9
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-07-07 16:42:49 +0530

    feat: add tests to create public & private room (#144)
    * feat: add tests for room_create
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * Update apidoc_room_create_test.go

---
+++ tests/apidoc_room_create_test.go
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/tidwall/gjson"
+
+	"github.com/matrix-org/complement/internal/b"
+	"github.com/matrix-org/complement/internal/client"
+	"github.com/matrix-org/complement/internal/match"
+	"github.com/matrix-org/complement/internal/must"
+)
+
+func TestRoomCreate(t *testing.T) {
+	deployment := Deploy(t, b.BlueprintAlice)
+	defer deployment.Destroy(t)
+	authedClient := deployment.Client(t, "hs1", "@alice:hs1")
+	t.Run("Parallel", func(t *testing.T) {
+		// sytest: POST /createRoom makes a public room
+		t.Run("POST /createRoom makes a public room", func(t *testing.T) {
+			t.Parallel()
+			reqBody := client.WithJSONBody(t, map[string]interface{}{
+				"visibility":      "public",
+				"room_alias_name": "30-room-create-alias-random",
+			})
+			res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
+			must.MatchResponse(t, res, match.HTTPResponse{
+				JSON: []match.JSON{
+					match.JSONKeyPresent("room_id"),
+					match.JSONKeyPresent("room_alias"),
+					match.JSONKeyTypeEqual("room_id", gjson.String),
+				},
+			})
+		})
+		// sytest: POST /createRoom makes a private room
+		t.Run("POST /createRoom makes a private room", func(t *testing.T) {
+			t.Parallel()
+			reqBody := client.WithJSONBody(t, map[string]interface{}{
+				"visibility": "private",
+			})
+			res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
+			must.MatchResponse(t, res, match.HTTPResponse{
+				JSON: []match.JSON{
+					match.JSONKeyPresent("room_id"),
+					match.JSONKeyTypeEqual("room_id", gjson.String),
+				},
+			})
+		})
+	})
+}
