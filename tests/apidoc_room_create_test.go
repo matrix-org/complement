fc048eb8c61cb61241bcf883ee78f7eba7da09de
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-07-12 19:41:16 +0530

    feat: add tests to check room_create errors (#155)
    * feat: add tests to check room_create errors
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_room_create_test.go
+++ tests/apidoc_room_create_test.go
@@ -46,5 +46,55 @@ func TestRoomCreate(t *testing.T) {
 				},
 			})
 		})
+		// sytest: POST /createRoom makes a private room with invites
+		t.Run("POST /createRoom makes a private room with invites", func(t *testing.T) {
+			t.Parallel()
+			userInvite := deployment.RegisterUser(t, "hs1", "create_room", "superuser")
+			reqBody := client.WithJSONBody(t, map[string]interface{}{
+				"visibility": "private",
+				"invite":     []string{userInvite.UserID},
+			})
+			res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
+			must.MatchResponse(t, res, match.HTTPResponse{
+				JSON: []match.JSON{
+					match.JSONKeyPresent("room_id"),
+					match.JSONKeyTypeEqual("room_id", gjson.String),
+				},
+			})
+		})
+		// sytest: POST /createRoom rejects attempts to create rooms with numeric versions
+		t.Run("POST /createRoom rejects attempts to create rooms with numeric versions", func(t *testing.T) {
+			t.Parallel()
+			reqBody := client.WithJSONBody(t, map[string]interface{}{
+				"visibility":   "private",
+				"room_version": 1,
+				"preset":       "public_chat",
+			})
+			res := authedClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
+			must.MatchResponse(t, res, match.HTTPResponse{
+				StatusCode: 400,
+				JSON: []match.JSON{
+					match.JSONKeyPresent("errcode"),
+					match.JSONKeyEqual("errcode", "M_BAD_JSON"),
+				},
+			})
+		})
+		// sytest: POST /createRoom rejects attempts to create rooms with unknown versions
+		t.Run("POST /createRoom rejects attempts to create rooms with unknown versions", func(t *testing.T) {
+			t.Parallel()
+			reqBody := client.WithJSONBody(t, map[string]interface{}{
+				"visibility":   "private",
+				"room_version": "ahfgwjyerhgiuveisbruvybseyrugvi",
+				"preset":       "public_chat",
+			})
+			res := authedClient.DoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
+			must.MatchResponse(t, res, match.HTTPResponse{
+				StatusCode: 400,
+				JSON: []match.JSON{
+					match.JSONKeyPresent("errcode"),
+					match.JSONKeyEqual("errcode", "M_UNSUPPORTED_ROOM_VERSION"),
+				},
+			})
+		})
 	})
 }
