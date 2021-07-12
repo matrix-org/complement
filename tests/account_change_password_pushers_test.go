adae8c1b87c325ce370c105b89238a2dd14dd129
Meenal Trivedi <79007582+meenal06@users.noreply.github.com>
2021-07-12 19:44:42 +0530

    feat: add change_password pushers test (#147)
    * feat: add change_password pushers test:
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * password fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * fix
    
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>
    
    * Update account_change_password_pushers_test.go
    
    * Update account_change_password_pushers_test.go
    
    * Update tests/account_change_password_pushers_test.go
    
    * Update tests/account_change_password_pushers_test.go
    
    Co-authored-by: kegsay <kegsay@gmail.com>

---
+++ tests/account_change_password_pushers_test.go
@@ -0,0 +1,95 @@
+// +build !dendrite_blacklist
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/matrix-org/complement/internal/b"
+	"github.com/matrix-org/complement/internal/client"
+	"github.com/matrix-org/complement/internal/match"
+	"github.com/matrix-org/complement/internal/must"
+
+	"github.com/tidwall/gjson"
+)
+
+func TestChangePasswordPushers(t *testing.T) {
+	deployment := Deploy(t, b.BlueprintAlice)
+	defer deployment.Destroy(t)
+	password1 := "superuser"
+	password2 := "my_new_password"
+	passwordClient := deployment.RegisterUser(t, "hs1", "test_change_password_pusher_user", password1)
+
+	// sytest: Pushers created with a different access token are deleted on password change
+	t.Run("Pushers created with a different access token are deleted on password change", func(t *testing.T) {
+		sessionOptional := createSession(t, deployment, passwordClient.UserID, password1)
+		reqBody := client.WithJSONBody(t, map[string]interface{}{
+			"data": map[string]interface{}{
+				"url": "https://dummy.url/_matrix/push/v1/notify",
+			},
+			"profile_tag":         "tag",
+			"kind":                "http",
+			"app_id":              "complement",
+			"app_display_name":    "complement_display_name",
+			"device_display_name": "device_display_name",
+			"pushkey":             "a_push_key",
+			"lang":                "en",
+		})
+
+		_ = sessionOptional.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "pushers", "set"}, reqBody)
+
+		changePassword(t, passwordClient, password1, password2)
+
+		pushersSize := 0
+
+		res := passwordClient.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "pushers"})
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+			JSON: []match.JSON{
+				match.JSONArrayEach("pushers", func(val gjson.Result) error {
+					pushersSize++
+					return nil
+				}),
+			},
+		})
+		if pushersSize != 0 {
+			t.Errorf("pushers size expected to be 0, found %d", pushersSize)
+		}
+	})
+
+	// sytest: Pushers created with a the same access token are not deleted on password change
+	t.Run("Pushers created with a the same access token are not deleted on password change", func(t *testing.T) {
+		reqBody := client.WithJSONBody(t, map[string]interface{}{
+			"data": map[string]interface{}{
+				"url": "https://dummy.url/_matrix/push/v1/notify",
+			},
+			"profile_tag":         "tag",
+			"kind":                "http",
+			"app_id":              "complement",
+			"app_display_name":    "complement_display_name",
+			"device_display_name": "device_display_name",
+			"pushkey":             "a_push_key",
+			"lang":                "en",
+		})
+
+		_ = passwordClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "pushers", "set"}, reqBody)
+
+		changePassword(t, passwordClient, password2, password1)
+
+		pushersSize := 0
+
+		res := passwordClient.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "pushers"})
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+			JSON: []match.JSON{
+				match.JSONArrayEach("pushers", func(val gjson.Result) error {
+					pushersSize++
+					return nil
+				}),
+			},
+		})
+		if pushersSize != 1 {
+			t.Errorf("pushers size expected to be 1, found %d", pushersSize)
+		}
+	})
+}
