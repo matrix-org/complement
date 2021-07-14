6f4f69b3f6f6cc9fb4a5464488171dc9632b6a1e
Meenal Trivedi <meenaltrivedi6102@gmail.com>
2021-07-14 23:58:34 +0530

    make requested changes
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/apidoc_room_state_test.go
+++ tests/apidoc_room_state_test.go
@@ -1,9 +1,6 @@
 package tests
 
 import (
-	"crypto/rand"
-	"io/ioutil"
-	"math/big"
 	"net/url"
 	"testing"
 
@@ -23,7 +20,10 @@ func TestRoomState(t *testing.T) {
 		// sytest: GET /rooms/:room_id/state/m.room.member/:user_id fetches my membership
 		t.Run("GET /rooms/:room_id/state/m.room.member/:user_id fetches my membership", func(t *testing.T) {
 			t.Parallel()
-			roomID, _ := roomFixture(t, authedClient)
+			roomID := authedClient.CreateRoom(t, map[string]interface{}{
+				"visibility": "public",
+				"preset":     "public_chat",
+			})
 			res := authedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "rooms", roomID, "state", "m.room.member", authedClient.UserID})
 			must.MatchResponse(t, res, match.HTTPResponse{
 				JSON: []match.JSON{
@@ -38,7 +38,10 @@ func TestRoomState(t *testing.T) {
 			t.Parallel()
 			queryParams := url.Values{}
 			queryParams.Set("format", "event")
-			roomID, _ := roomFixture(t, authedClient)
+			roomID := authedClient.CreateRoom(t, map[string]interface{}{
+				"visibility": "public",
+				"preset":     "public_chat",
+			})
 			res := authedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "rooms", roomID, "state", "m.room.member", authedClient.UserID}, client.WithQueries(queryParams))
 			must.MatchResponse(t, res, match.HTTPResponse{
 				JSON: []match.JSON{
@@ -54,7 +57,11 @@ func TestRoomState(t *testing.T) {
 		// sytest: GET /rooms/:room_id/state/m.room.power_levels fetches powerlevels
 		t.Run("GET /rooms/:room_id/state/m.room.power_levels fetches powerlevels", func(t *testing.T) {
 			t.Parallel()
-			roomID, _ := roomFixture(t, authedClient)
+			roomID := authedClient.CreateRoom(t, map[string]interface{}{
+				"visibility":      "public",
+				"preset":          "public_chat",
+				"room_alias_name": "room_alias",
+			})
 			res := authedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "rooms", roomID, "state", "m.room.power_levels"})
 			must.MatchResponse(t, res, match.HTTPResponse{
 				JSON: []match.JSON{
@@ -72,40 +79,40 @@ func TestRoomState(t *testing.T) {
 	})
 }
 
-func roomFixture(t *testing.T, authedClient *client.CSAPI) (roomID string, roomAlias string) {
-	t.Helper()
-	roomAlias, err := generateRoomAlias()
-	if err != nil {
-		t.Fatalf("unable to generate room alias: %v", err)
-	}
-	reqBody := client.WithJSONBody(t, map[string]interface{}{
-		"visibility":      "public",
-		"preset":          "public_chat",
-		"room_alias_name": roomAlias,
-	})
-
-	res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatalf("unable to read response body: %v", err)
-	}
-
-	roomID = gjson.GetBytes(body, "room_id").Str
-	roomAlias = gjson.GetBytes(body, "room_alias").Str
-	return roomID, roomAlias
-}
-
-func generateRoomAlias() (string, error) {
-	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
-	ret := make([]byte, 32)
-	for i := 0; i < 32; i++ {
-		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
-		if err != nil {
-			return "", err
-		}
-		ret[i] = letters[num.Int64()]
-	}
-
-	return string(ret), nil
-}
+//func roomFixture(t *testing.T, authedClient *client.CSAPI) (roomID string, roomAlias string) {
+//	t.Helper()
+//	roomAlias, err := generateRoomAlias()
+//	if err != nil {
+//		t.Fatalf("unable to generate room alias: %v", err)
+//	}
+//	reqBody := client.WithJSONBody(t, map[string]interface{}{
+//		"visibility":      "public",
+//		"preset":          "public_chat",
+//		"room_alias_name": roomAlias,
+//	})
+//
+//	res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "createRoom"}, reqBody)
+//
+//	body, err := ioutil.ReadAll(res.Body)
+//	if err != nil {
+//		t.Fatalf("unable to read response body: %v", err)
+//	}
+//
+//	roomID = gjson.GetBytes(body, "room_id").Str
+//	roomAlias = gjson.GetBytes(body, "room_alias").Str
+//	return roomID, roomAlias
+//}
+//
+//func generateRoomAlias() (string, error) {
+//	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+//	ret := make([]byte, 32)
+//	for i := 0; i < 32; i++ {
+//		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+//		if err != nil {
+//			return "", err
+//		}
+//		ret[i] = letters[num.Int64()]
+//	}
+//
+//	return string(ret), nil
+//}
