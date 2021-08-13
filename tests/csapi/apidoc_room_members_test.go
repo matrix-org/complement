f1684932eb0ad4b81b3909caf6b95b259460e4e9
Meenal Trivedi <meenaltrivedi6102@gmail.com>
2021-08-14 01:21:26 +0530

    fix: use syncUntill logic
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/csapi/apidoc_room_members_test.go
+++ tests/csapi/apidoc_room_members_test.go
@@ -11,19 +11,20 @@ import (
 )
 
 func TestRoomMembers(t *testing.T) {
-	deployment := Deploy(t, b.BlueprintAlice)
+	deployment := Deploy(t, b.BlueprintOneToOneRoom)
 	defer deployment.Destroy(t)
-	authedClient := deployment.Client(t, "hs1", "@alice:hs1")
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+	bob := deployment.Client(t, "hs1", "@bob:hs1")
 	t.Run("Parallel", func(t *testing.T) {
 		// sytest: POST /rooms/:room_id/join can join a room
 		t.Run("POST /rooms/:room_id/join can join a room", func(t *testing.T) {
 			t.Parallel()
-			roomID := authedClient.CreateRoom(t, map[string]interface{}{
+			roomID := alice.CreateRoom(t, map[string]interface{}{
 				"visibility": "public",
 				"preset":     "public_chat",
 			})
 
-			res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "rooms", roomID, "join"})
+			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "rooms", roomID, "join"})
 
 			must.MatchResponse(t, res, match.HTTPResponse{
 				JSON: []match.JSON{
@@ -33,25 +34,28 @@ func TestRoomMembers(t *testing.T) {
 				},
 			})
 
-			res = authedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "rooms", roomID, "state", "m.room.member", "@alice:hs1"})
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("membership"),
-					match.JSONKeyTypeEqual("membership", gjson.String),
-					match.JSONKeyEqual("membership", "join"),
+			bob.SyncUntilTimelineHas(
+				t,
+				roomID,
+				func(ev gjson.Result) bool {
+					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+						return false
+					}
+					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+					return true
 				},
-			})
+			)
 		})
 		// sytest: POST /join/:room_alias can join a room
 		t.Run("POST /join/:room_alias can join a room", func(t *testing.T) {
 			t.Parallel()
-			roomID := authedClient.CreateRoom(t, map[string]interface{}{
+			roomID := alice.CreateRoom(t, map[string]interface{}{
 				"visibility":      "public",
 				"preset":          "public_chat",
 				"room_alias_name": "room_alias_random",
 			})
 
-			res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", "#room_alias_random:hs1"})
+			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", "#room_alias_random:hs1"})
 
 			must.MatchResponse(t, res, match.HTTPResponse{
 				JSON: []match.JSON{
@@ -61,24 +65,27 @@ func TestRoomMembers(t *testing.T) {
 				},
 			})
 
-			res = authedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "rooms", roomID, "state", "m.room.member", "@alice:hs1"})
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("membership"),
-					match.JSONKeyTypeEqual("membership", gjson.String),
-					match.JSONKeyEqual("membership", "join"),
+			bob.SyncUntilTimelineHas(
+				t,
+				roomID,
+				func(ev gjson.Result) bool {
+					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+						return false
+					}
+					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+					return true
 				},
-			})
+			)
 		})
 		// sytest: POST /join/:room_id can join a room
 		t.Run("POST /join/:room_id can join a room", func(t *testing.T) {
 			t.Parallel()
-			roomID := authedClient.CreateRoom(t, map[string]interface{}{
+			roomID := alice.CreateRoom(t, map[string]interface{}{
 				"visibility": "public",
 				"preset":     "public_chat",
 			})
 
-			res := authedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", roomID})
+			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", roomID})
 
 			must.MatchResponse(t, res, match.HTTPResponse{
 				JSON: []match.JSON{
@@ -88,14 +95,17 @@ func TestRoomMembers(t *testing.T) {
 				},
 			})
 
-			res = authedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "rooms", roomID, "state", "m.room.member", "@alice:hs1"})
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("membership"),
-					match.JSONKeyTypeEqual("membership", gjson.String),
-					match.JSONKeyEqual("membership", "join"),
+			bob.SyncUntilTimelineHas(
+				t,
+				roomID,
+				func(ev gjson.Result) bool {
+					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+						return false
+					}
+					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+					return true
 				},
-			})
+			)
 		})
 	})
 }
