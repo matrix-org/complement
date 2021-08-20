930eb760bf6f332201be5a89c63a0bdb0e2267e0
Meenal Trivedi <meenaltrivedi6102@gmail.com>
2021-08-21 01:30:47 +0530

    use sync_until
    Signed-off-by: Meenal Trivedi <meenaltrivedi6102@gmail.com>

--- tests/csapi/apidoc_room_members_test.go
+++ tests/csapi/apidoc_room_members_test.go
@@ -17,168 +17,168 @@ func TestRoomMembers(t *testing.T) {
 	alice := deployment.Client(t, "hs1", "@alice:hs1")
 	bob := deployment.Client(t, "hs1", "@bob:hs1")
 	t.Run("Parallel", func(t *testing.T) {
-		// sytest: POST /rooms/:room_id/join can join a room
-		t.Run("POST /rooms/:room_id/join can join a room", func(t *testing.T) {
-			t.Parallel()
-			roomID := alice.CreateRoom(t, map[string]interface{}{
-				"visibility": "public",
-				"preset":     "public_chat",
-			})
-
-			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "rooms", roomID, "join"})
-
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("room_id"),
-					match.JSONKeyTypeEqual("room_id", gjson.String),
-					match.JSONKeyEqual("room_id", roomID),
-				},
-			})
-
-			bob.SyncUntilTimelineHas(
-				t,
-				roomID,
-				func(ev gjson.Result) bool {
-					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
-						return false
-					}
-					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
-					return true
-				},
-			)
-		})
-		// sytest: POST /join/:room_alias can join a room
-		t.Run("POST /join/:room_alias can join a room", func(t *testing.T) {
-			t.Parallel()
-			roomID := alice.CreateRoom(t, map[string]interface{}{
-				"visibility":      "public",
-				"preset":          "public_chat",
-				"room_alias_name": "room_alias_random",
-			})
-
-			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", "#room_alias_random:hs1"})
-
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("room_id"),
-					match.JSONKeyTypeEqual("room_id", gjson.String),
-					match.JSONKeyEqual("room_id", roomID),
-				},
-			})
-
-			bob.SyncUntilTimelineHas(
-				t,
-				roomID,
-				func(ev gjson.Result) bool {
-					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
-						return false
-					}
-					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
-					return true
-				},
-			)
-		})
-		// sytest: POST /join/:room_id can join a room
-		t.Run("POST /join/:room_id can join a room", func(t *testing.T) {
-			t.Parallel()
-			roomID := alice.CreateRoom(t, map[string]interface{}{
-				"visibility": "public",
-				"preset":     "public_chat",
-			})
-
-			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", roomID})
-
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("room_id"),
-					match.JSONKeyTypeEqual("room_id", gjson.String),
-					match.JSONKeyEqual("room_id", roomID),
-				},
-			})
-
-			bob.SyncUntilTimelineHas(
-				t,
-				roomID,
-				func(ev gjson.Result) bool {
-					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
-						return false
-					}
-					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
-					return true
-				},
-			)
-		})
-		// sytest: POST /join/:room_id can join a room with custom content
-		t.Run("POST /join/:room_id can join a room with custom content", func(t *testing.T) {
-			t.Parallel()
-			roomID := alice.CreateRoom(t, map[string]interface{}{
-				"visibility": "public",
-				"preset":     "public_chat",
-			})
-
-			reqBody := client.WithJSONBody(t, map[string]interface{}{
-				"foo": "bar",
-			})
-
-			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", roomID}, reqBody)
-
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("room_id"),
-					match.JSONKeyTypeEqual("room_id", gjson.String),
-					match.JSONKeyEqual("room_id", roomID),
-				},
-			})
-
-			bob.SyncUntilTimelineHas(
-				t,
-				roomID,
-				func(ev gjson.Result) bool {
-					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
-						return false
-					}
-					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
-					must.EqualStr(t, ev.Get("content").Get("foo").Str, "bar", "Failed to propagate custom content")
-					return true
-				},
-			)
-		})
-		// sytest: POST /join/:room_alias can join a room with custom content
-		t.Run("POST /join/:room_alias can join a room with custom content", func(t *testing.T) {
-			t.Parallel()
-			roomID := alice.CreateRoom(t, map[string]interface{}{
-				"visibility":      "public",
-				"preset":          "public_chat",
-				"room_alias_name": "room_alias_random_2",
-			})
-
-			reqBody := client.WithJSONBody(t, map[string]interface{}{
-				"foo": "bar",
-			})
-
-			res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", "#room_alias_random_2:hs1"}, reqBody)
-
-			must.MatchResponse(t, res, match.HTTPResponse{
-				JSON: []match.JSON{
-					match.JSONKeyPresent("room_id"),
-					match.JSONKeyTypeEqual("room_id", gjson.String),
-					match.JSONKeyEqual("room_id", roomID),
-				},
-			})
-
-			bob.SyncUntilTimelineHas(
-				t,
-				roomID,
-				func(ev gjson.Result) bool {
-					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
-						return false
-					}
-					must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
-					must.EqualStr(t, ev.Get("content").Get("foo").Str, "bar", "Failed to propagate custom content")
-					return true
-				},
-			)
-		})
+		//// sytest: POST /rooms/:room_id/join can join a room
+		//t.Run("POST /rooms/:room_id/join can join a room", func(t *testing.T) {
+		//	t.Parallel()
+		//	roomID := alice.CreateRoom(t, map[string]interface{}{
+		//		"visibility": "public",
+		//		"preset":     "public_chat",
+		//	})
+		//
+		//	res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "rooms", roomID, "join"})
+		//
+		//	must.MatchResponse(t, res, match.HTTPResponse{
+		//		JSON: []match.JSON{
+		//			match.JSONKeyPresent("room_id"),
+		//			match.JSONKeyTypeEqual("room_id", gjson.String),
+		//			match.JSONKeyEqual("room_id", roomID),
+		//		},
+		//	})
+		//
+		//	bob.SyncUntilTimelineHas(
+		//		t,
+		//		roomID,
+		//		func(ev gjson.Result) bool {
+		//			if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+		//				return false
+		//			}
+		//			must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+		//			return true
+		//		},
+		//	)
+		//})
+		//// sytest: POST /join/:room_alias can join a room
+		//t.Run("POST /join/:room_alias can join a room", func(t *testing.T) {
+		//	t.Parallel()
+		//	roomID := alice.CreateRoom(t, map[string]interface{}{
+		//		"visibility":      "public",
+		//		"preset":          "public_chat",
+		//		"room_alias_name": "room_alias_random",
+		//	})
+		//
+		//	res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", "#room_alias_random:hs1"})
+		//
+		//	must.MatchResponse(t, res, match.HTTPResponse{
+		//		JSON: []match.JSON{
+		//			match.JSONKeyPresent("room_id"),
+		//			match.JSONKeyTypeEqual("room_id", gjson.String),
+		//			match.JSONKeyEqual("room_id", roomID),
+		//		},
+		//	})
+		//
+		//	bob.SyncUntilTimelineHas(
+		//		t,
+		//		roomID,
+		//		func(ev gjson.Result) bool {
+		//			if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+		//				return false
+		//			}
+		//			must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+		//			return true
+		//		},
+		//	)
+		//})
+		//// sytest: POST /join/:room_id can join a room
+		//t.Run("POST /join/:room_id can join a room", func(t *testing.T) {
+		//	t.Parallel()
+		//	roomID := alice.CreateRoom(t, map[string]interface{}{
+		//		"visibility": "public",
+		//		"preset":     "public_chat",
+		//	})
+		//
+		//	res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", roomID})
+		//
+		//	must.MatchResponse(t, res, match.HTTPResponse{
+		//		JSON: []match.JSON{
+		//			match.JSONKeyPresent("room_id"),
+		//			match.JSONKeyTypeEqual("room_id", gjson.String),
+		//			match.JSONKeyEqual("room_id", roomID),
+		//		},
+		//	})
+		//
+		//	bob.SyncUntilTimelineHas(
+		//		t,
+		//		roomID,
+		//		func(ev gjson.Result) bool {
+		//			if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+		//				return false
+		//			}
+		//			must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+		//			return true
+		//		},
+		//	)
+		//})
+		//// sytest: POST /join/:room_id can join a room with custom content
+		//t.Run("POST /join/:room_id can join a room with custom content", func(t *testing.T) {
+		//	t.Parallel()
+		//	roomID := alice.CreateRoom(t, map[string]interface{}{
+		//		"visibility": "public",
+		//		"preset":     "public_chat",
+		//	})
+		//
+		//	reqBody := client.WithJSONBody(t, map[string]interface{}{
+		//		"foo": "bar",
+		//	})
+		//
+		//	res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", roomID}, reqBody)
+		//
+		//	must.MatchResponse(t, res, match.HTTPResponse{
+		//		JSON: []match.JSON{
+		//			match.JSONKeyPresent("room_id"),
+		//			match.JSONKeyTypeEqual("room_id", gjson.String),
+		//			match.JSONKeyEqual("room_id", roomID),
+		//		},
+		//	})
+		//
+		//	bob.SyncUntilTimelineHas(
+		//		t,
+		//		roomID,
+		//		func(ev gjson.Result) bool {
+		//			if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+		//				return false
+		//			}
+		//			must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+		//			must.EqualStr(t, ev.Get("content").Get("foo").Str, "bar", "Failed to propagate custom content")
+		//			return true
+		//		},
+		//	)
+		//})
+		//// sytest: POST /join/:room_alias can join a room with custom content
+		//t.Run("POST /join/:room_alias can join a room with custom content", func(t *testing.T) {
+		//	t.Parallel()
+		//	roomID := alice.CreateRoom(t, map[string]interface{}{
+		//		"visibility":      "public",
+		//		"preset":          "public_chat",
+		//		"room_alias_name": "room_alias_random_2",
+		//	})
+		//
+		//	reqBody := client.WithJSONBody(t, map[string]interface{}{
+		//		"foo": "bar",
+		//	})
+		//
+		//	res := bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "join", "#room_alias_random_2:hs1"}, reqBody)
+		//
+		//	must.MatchResponse(t, res, match.HTTPResponse{
+		//		JSON: []match.JSON{
+		//			match.JSONKeyPresent("room_id"),
+		//			match.JSONKeyTypeEqual("room_id", gjson.String),
+		//			match.JSONKeyEqual("room_id", roomID),
+		//		},
+		//	})
+		//
+		//	bob.SyncUntilTimelineHas(
+		//		t,
+		//		roomID,
+		//		func(ev gjson.Result) bool {
+		//			if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
+		//				return false
+		//			}
+		//			must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "Bob failed to join the room")
+		//			must.EqualStr(t, ev.Get("content").Get("foo").Str, "bar", "Failed to propagate custom content")
+		//			return true
+		//		},
+		//	)
+		//})
 		// sytest: POST /rooms/:room_id/leave can leave a room
 		t.Run("POST /rooms/:room_id/leave can leave a room", func(t *testing.T) {
 			t.Parallel()
@@ -211,9 +211,11 @@ func TestRoomMembers(t *testing.T) {
 
 			_ = bob.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "rooms", roomID, "leave"})
 
-			bob.SyncUntilTimelineHas(
+			bob.SyncUntil(
 				t,
-				roomID,
+				"",
+				"",
+				"rooms.leave."+client.GjsonEscape(roomID)+".timeline.events",
 				func(ev gjson.Result) bool {
 					if ev.Get("type").Str != "m.room.member" || ev.Get("state_key").Str != bob.UserID {
 						return false
