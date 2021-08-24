ce8f3a9c54a6b416bbef021273b7637eb6247b00
David Robertson <david.m.robertson1@gmail.com>
2021-08-24 18:05:13 +0100

    Update tests after review
    hopefully will now pass on dendrite

--- tests/csapi/user_directory_display_names_test.go
+++ tests/csapi/user_directory_display_names_test.go
@@ -7,58 +7,70 @@ import (
 	"testing"
 
 	"github.com/matrix-org/complement/internal/b"
+	"github.com/matrix-org/complement/internal/client"
 	"github.com/matrix-org/complement/internal/match"
 	"github.com/matrix-org/complement/internal/must"
 )
 
 func TestRoomSpecificUsernameNotLeaked(t *testing.T) {
 	// Reproduces https://github.com/matrix-org/synapse/issues/5677
-	// In that bug report, Alice has revealed a private name to a friend X,
-	// and Bob can see that private name when he shouldn't be able to.
-	// I've tweaked the names to be more traditional: Alice reveals a private name
-	// to Bob, and Eve shouldn't be able to see that name.
+	// In that bug report,
+	// - Bob knows about Alice, and
+	// - Alice has revealed a private name to another friend X,
+	// - Bob can see that private name when he shouldn't be able to.
+	//
+	// I've tweaked the names to be more traditional:
+	// - Even knows about Alice,
+	// - Alice reveals a private name to another friend Bob
+	// - Eve shouldn't be able to see that private name.
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
 
 	alice := deployment.Client(t, "hs1", "@alice:hs1")
-	bob := deployment.RegisterUser(t, "hs1", "bob", "bob-pw")
-	eve := deployment.RegisterUser(t, "hs1", "eve", "eve-pw")
+	bob := deployment.RegisterUser(t, "hs1", "bob", "bob-has-a-very-secret-pw")
+	eve := deployment.RegisterUser(t, "hs1", "eve", "eve-has-a-very-secret-pw")
 
-	t.Run("Usernames specific to a room aren't leaked in the user directory", func(t *testing.T) {
-		// Bob creates a new room and invites Alice. She accepts.
-		privateRoom := bob.CreateRoom(t, map[string]interface{}{
-			"m.federate": false,
-		})
-		bob.InviteRoom(t, privateRoom, "@alice:hs1")
-		alice.JoinRoom(t, privateRoom, nil)
+	// Alice creates a public room (so Eve can see that Alice exists)
+	alice.CreateRoom(t, map[string]interface{}{"visibility": "public"})
 
-		// Alice reveals her private name to Bob
-		alice.MustDo(
-			t,
-			"PUT",
-			[]string{"_matrix", "client", "r0", "rooms", privateRoom, "state", "m.room.member",
-				"@alice:hs1"},
-			map[string]interface{}{
-				"displayname": "Alice Cooper",
-				"membership":  "join",
-			},
-		)
+	// Bob creates a new room and invites Alice.
+	privateRoom := bob.CreateRoom(t, map[string]interface{}{
+		"visibility": "private",
+		"invite":     []string{alice.UserID},
+	})
 
-		// Eve looks up alice in the directory using her public name
-		res := eve.MustDo(
-			t,
-			"POST",
-			[]string{"_matrix", "client", "r0", "user_directory", "search"},
-			map[string]interface{}{
-				"search_term": "alice",
-			},
-		)
+	// Alice waits until she sees the invite, then accepts.
+	alice.SyncUntilInvitedTo(t, privateRoom)
+	alice.JoinRoom(t, privateRoom, nil)
 
+	// Alice reveals her private name to Bob
+	alice.MustDoFunc(
+		t,
+		"PUT",
+		[]string{"_matrix", "client", "r0", "rooms", privateRoom, "state", "m.room.member", "@alice:hs1"},
+		client.WithJSONBody(t, map[string]interface{}{
+			"displayname": "Alice Cooper",
+			"membership":  "join",
+		}),
+	)
+
+	// Eve should be able to find alice by her public name and mxid
+	for _, name := range []string{"alice", alice.UserID} {
+		res := eve.SearchUserDirectory(t, name)
 		must.MatchResponse(t, res, match.HTTPResponse{
 			JSON: []match.JSON{
+				match.JSONKeyArrayOfSize("results", 1),
 				match.JSONKeyEqual("results.0.display_name", "alice"),
-				match.JSONKeyEqual("results.0.user_id", "@alice:hs1"),
+				match.JSONKeyEqual("results.0.user_id", alice.UserID),
 			},
 		})
+	}
+
+	// Eve should not be able to find Alice by her private name
+	res := eve.SearchUserDirectory(t, "Alice Cooper")
+	must.MatchResponse(t, res, match.HTTPResponse{
+		JSON: []match.JSON{
+			match.JSONKeyArrayOfSize("results", 0),
+		},
 	})
 }
