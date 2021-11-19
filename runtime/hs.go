1a954ab6f4b946bc6efd6c7615729e7f2b5b0cb9
Kegan Dougal <kegan@matrix.org>
2021-11-19 13:46:19 +0000

    Add ability to skip individual tests
    - Added `runtime.SkipIf`
    - Re-combined federation test as it can now be individually skipped on Dendrite.
    
    From a CI perspective, no changes are required as this uses the existing build
    tags setup to detect HS impl.

---
+++ runtime/hs.go
@@ -0,0 +1,34 @@
+package runtime
+
+import "testing"
+
+const (
+	Dendrite = "dendrite"
+	Synapse  = "synapse"
+)
+
+var Homeserver string
+
+// Skip the test (via t.Skipf) if the homeserver being tested matches `hs`, else return.
+//
+// The homeserver being tested is detected via the presence of a `*_blacklist` tag e.g:
+//   go test -tags="dendrite_blacklist"
+// This means it is important to always specify this tag when running tests. Failure to do
+// so will result in a warning being printed to stdout, and the test will be run. When a new server
+// implementation is added, a respective `hs_$name.go` needs to be created in this directory. This
+// file pairs together the tag name with a string constant declared in this package
+// e.g. dendrite_blacklist == runtime.Dendrite
+func SkipIf(t *testing.T, hs string) {
+	t.Helper()
+	if Homeserver == hs {
+		t.Skipf("skipped on %s", hs)
+	}
+	if Homeserver == "" {
+		// they ran Complement without a blacklist so it's impossible to know what HS they are
+		// running, warn them.
+		t.Logf(
+			"WARNING: %s called runtime.SkipIf(%s) but Complement doesn't know which HS is running as it was run without a *_blacklist tag: executing test.",
+			t.Name(), hs,
+		)
+	}
+}
