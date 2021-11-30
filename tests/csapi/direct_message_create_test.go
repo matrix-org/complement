0df4990624e12842f9c68663ea6758614b9413f1
David Robertson <davidr@element.io>
2021-11-30 12:06:25 +0000

    Test name and description

--- tests/csapi/direct_message_create_test.go
+++ tests/csapi/direct_message_create_test.go
@@ -9,7 +9,17 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-func TestDirectMessageCreate(t *testing.T) {
+// Synapse has a long-standing bug
+//     https://github.com/matrix-org/synapse/issues/9768
+// where membership events are duplicated in the timeline
+// returned by a call to `/sync` with a `since` parameter.
+// (AFAICS the spec doesn't mandate that there are no such
+// duplicates, but pretty much everyone expects things to
+// work that way.)
+//
+// This test reproduces the duplicated membership event for
+// Bob after Alice invites him to a 1-1 room.
+func TestMembershipNotDuplicatedWhenJoiningDirectMessage(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintOneToOneRoom)
 	defer deployment.Destroy(t)
 	alice := deployment.Client(t, "hs1", "@alice:hs1")
