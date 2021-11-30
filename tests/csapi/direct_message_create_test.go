71fbecd05ba6fc7fbadbe932b202fc833c5d9e63
David Robertson <davidr@element.io>
2021-11-30 14:31:43 +0000

    Use sentinel event and describe the test steps

--- tests/csapi/direct_message_create_test.go
+++ tests/csapi/direct_message_create_test.go
@@ -1,7 +1,6 @@
 package csapi_tests
 
 import (
-	"net/url"
 	"testing"
 
 	"github.com/tidwall/gjson"
@@ -32,35 +31,43 @@ func TestMembershipNotDuplicatedWhenJoiningDirectMessage(t *testing.T) {
 		"is_direct": true,
 	})
 
-	// Bob receives the invite and joins.
+	// Bob receives the invite and joins. Extract the
+	// `next_batch` from the response for use later.
 	nextBatch := bob.SyncUntilInvitedTo(t, roomID)
 	bob.JoinRoom(t, roomID, []string{"hs1"})
 
-	// Wait until Bob sees that he's joined.
-	isBobJoinEvent := func(ev gjson.Result) bool {
-		return ev.Get("type").Str == "m.room.member" && ev.Get("state_key").Str == bob.UserID && ev.Get("content").Get("membership").Str == "join"
-	}
-	bob.SyncUntilTimelineHas(t, roomID, isBobJoinEvent)
+	// To reproduce the bug, we ought to sync until we see
+	// a duplicate membership event. But actually seeing
+	// that event should fail the test, so we shouldn't
+	// expect it to happen. To account for both cases,
+	// send a dummy sentinel event after we've joined.
+	SENTINEL_EVENT_TYPE := "com.example.dummy"
+	bob.SendEventSynced(t, roomID, b.Event{
+		Type:    SENTINEL_EVENT_TYPE,
+		Sender:  bob.UserID,
+		Content: map[string]interface{}{},
+	})
 
-	// Repeat the sync, but in a form where we can inspect the output.
-	query := url.Values{
-		"timeout": []string{"1000"},
-		"since":   []string{nextBatch},
-	}
-
-	res := bob.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "sync"}, client.WithQueries(query))
-	body := client.ParseJSON(t, res)
-	eventRes := gjson.GetBytes(body, "rooms.join").Get(roomID).Get("timeline.events")
-	if !eventRes.IsArray() {
-		t.Fatal("events in timeline was not an array: ", eventRes.Raw)
-	}
-	events := eventRes.Array()
+	// Replay a sync from just before we joined until we see
+	// the sentinel event. Count the number of Join events we
+	// see.
 	bobJoinEvents := 0
-	for _, ev := range events {
-		if isBobJoinEvent(ev) {
-			bobJoinEvents += 1
-		}
-	}
+	bob.SyncUntil(
+		t,
+		nextBatch,
+		"",
+		"rooms.join."+client.GjsonEscape(roomID)+".timeline.events",
+		func(ev gjson.Result) bool {
+			if ev.Get("type").Str == "m.room.member" &&
+				ev.Get("state_key").Str == bob.UserID &&
+				ev.Get("content.membership").Str == "join" {
+				bobJoinEvents += 1
+			}
+
+			return ev.Get("type").Str == SENTINEL_EVENT_TYPE
+		},
+	)
+
 	if bobJoinEvents != 1 {
 		t.Fatalf("Saw %d join events for Bob; expected 1", bobJoinEvents)
 	}
