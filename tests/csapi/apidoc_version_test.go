8ac2f7e0b8ccdfdee73f23e86c3e3864627e519d
Jonathan de Jong <jonathan@automatia.nl>
2021-12-02 17:50:57 +0100

    Improve versions test

--- tests/csapi/apidoc_version_test.go
+++ tests/csapi/apidoc_version_test.go
@@ -2,6 +2,7 @@ package csapi_tests
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/tidwall/gjson"
@@ -14,10 +15,20 @@ import (
 func TestVersionStructure(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
-	unauthedClient := deployment.Client(t, "hs1", "")
+
+	client := deployment.Client(t, "hs1", "")
+
 	// sytest: Version responds 200 OK with valid structure
 	t.Run("Version responds 200 OK with valid structure", func(t *testing.T) {
-		res := unauthedClient.MustDo(t, "GET", []string{"_matrix", "client", "versions"}, nil)
+		res := client.MustDoFunc(t, "GET", []string{"_matrix", "client", "versions"})
+
+		// Matches;
+		// - r0.?.?
+		//  where ? is any single digit
+		// - v1^.*
+		//  where 1^ is 1 through 9 for the first digit, then any digit thereafter,
+		//  and * is any single or multiple of digits
+		versionRegex, _ := regexp.Compile(`^(v[1-9]\d*\.\d+|r0\.\d\.\d)$`)
 
 		must.MatchResponse(t, res, match.HTTPResponse{
 			JSON: []match.JSON{
@@ -26,8 +37,22 @@ func TestVersionStructure(t *testing.T) {
 					if val.Type != gjson.String {
 						return fmt.Errorf("'versions' value is not a string: %s", val.Raw)
 					}
+					if !versionRegex.MatchString(val.Str) {
+						return fmt.Errorf("'versions' value did not match version regex: %s", val.Str)
+					}
 					return nil
 				}),
+				// Check when unstable_features is present if it's an object
+				func(body []byte) error {
+					res := gjson.GetBytes(body, "unstable_features")
+					if !res.Exists() {
+						return nil
+					}
+					if !res.IsObject() {
+						return fmt.Errorf("unstable_features was present, and wasn't an object")
+					}
+					return nil
+				},
 			},
 		})
 	})
