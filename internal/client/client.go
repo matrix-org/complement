908f4d1ceec82cf89a5ac3dc5863096f4dff8ede
David Robertson <davidr@element.io>
2021-12-06 13:26:08 +0000

    SyncUntilGlobalAccountDataHas
    Maybe this doesn't need a helper though?

--- internal/client/client.go
+++ internal/client/client.go
@@ -141,6 +141,18 @@ func (c *CSAPI) SyncUntilTimelineHas(t *testing.T, roomID string, check func(gjs
 	return c.SyncUntil(t, "", "", "rooms.join."+GjsonEscape(roomID)+".timeline.events", check)
 }
 
+// SyncUntilGlobalAccountDataHas is a wrapper around `SyncUntil`.
+// It blocks and continually calls `/sync` until
+// - we an event in the global account data for which the `check` function returns True
+// If the `check` function fails the test, the failing event will be automatically logged.
+// Will time out after CSAPI.SyncUntilTimeout.
+//
+// Returns the `next_batch` token from the last /sync response. This can be passed as
+// `since` to sync from this point forward only.
+func (c *CSAPI) SyncUntilGlobalAccountDataHas(t *testing.T, check func(gjson.Result) bool) string {
+	t.Helper()
+	return c.SyncUntil(t, "", "", "account_data.events", check)
+}
 // SyncUntilInvitedTo is a wrapper around SyncUntil.
 // It blocks and continually calls `/sync` until we've been invited to the given room.
 // Will time out after CSAPI.SyncUntilTimeout.
