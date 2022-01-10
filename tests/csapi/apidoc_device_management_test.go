07d0c918a8b14fcd11a30907d47c0df53e3438e9
David Robertson <davidr@element.io>
2022-01-10 16:32:34 +0000

    WIP test case

--- tests/csapi/apidoc_device_management_test.go
+++ tests/csapi/apidoc_device_management_test.go
@@ -1,6 +1,8 @@
 package csapi_tests
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/tidwall/gjson"
@@ -120,4 +122,50 @@ func TestDeviceManagement(t *testing.T) {
 			StatusCode: 404,
 		})
 	})
+}
+
+// Alice and Bob are in a room, each on their own homeserver.
+// Alice repeatedly creates a new device.
+// For each new device, we check that
+//  - Bob's sync response indicates that Alice's device list has changed.
+//  - If Bob fetches Alice's keys then he sees the entire list.
+// REVIEW: should this be here or in a new file?
+func TestManyDevices(t *testing.T) {
+	deployment := Deploy(t, b.BlueprintFederationOneToOneRoom)
+	defer deployment.Destroy(t)
+
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+	bob := deployment.Client(t, "hs2", "@bob:hs2")
+
+	loginAliceOnNewDevice := func(i int) *http.Response {
+		alicePassword := "complement_meets_min_pasword_req_alice"
+		return alice.MustDoFunc(
+			t,
+			"POST",
+			[]string{"_matrix", "client", "v3", "login"},
+			client.WithJSONBody(t, map[string]interface{}{
+				"device_id":                   fmt.Sprintf("alice_%d", i),
+				"initial_device_display_name": fmt.Sprintf("Alice device %d", i),
+				"identifier": map[string]interface{}{
+					"type": "m.id.user",
+					"user": "alice",
+				},
+				"password": alicePassword,
+				"type":     "m.login.password",
+			},
+			))
+	}
+
+	for i := 1; i <= 500; i += 1 {
+		// Alice logs in to make a new device.
+		loginAliceOnNewDevice(i)
+
+		// We wait for Bob to see that Alice's device list has changed
+		bob.MustSyncUntil(
+			t,
+			client.SyncReq{},
+			client.SyncUserHasChangedDevices(alice.UserID),
+		)
+		// Bob fetches Alice's device list
+	}
 }
