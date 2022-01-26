461917d45520a80c1d78e60381909cdb63aae20f
Kegan Dougal <kegan@matrix.org>
2022-01-26 13:09:47 +0000

    Test comment 2

--- internal/docker/volumes.go
+++ internal/docker/volumes.go
@@ -10,7 +10,7 @@ import (
 	client "github.com/docker/docker/client"
 )
 
-// test
+// test 2
 // A volume is a mounted directory on the homeserver container
 type Volume interface {
 	// Return the mount point
