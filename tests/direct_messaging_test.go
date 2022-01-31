fdb25051b6bca990f8bbbe54d2bc91b66f2662fc
Kegan Dougal <kegan@matrix.org>
2022-01-31 13:06:37 +0000

    Add more DM tests

--- tests/direct_messaging_test.go
+++ tests/direct_messaging_test.go
@@ -8,11 +8,66 @@ import (
 	"github.com/matrix-org/complement/internal/b"
 	"github.com/matrix-org/complement/internal/client"
 	"github.com/matrix-org/complement/internal/federation"
+	"github.com/matrix-org/complement/internal/match"
+	"github.com/matrix-org/complement/internal/must"
 
 	"github.com/matrix-org/gomatrixserverlib"
 	"github.com/tidwall/gjson"
 )
 
+// Test that a client can write `m.direct` account data and get told about updates to that event.
+// Requires a functioning account data implementation.
+func TestWriteMDirectAccountData(t *testing.T) {
+	deployment := Deploy(t, b.BlueprintOneToOneRoom)
+	defer deployment.Destroy(t)
+
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+	bob := deployment.Client(t, "hs1", "@bob:hs1")
+	roomID := alice.CreateRoom(t, map[string]interface{}{
+		"invite":    []string{bob.UserID},
+		"is_direct": true,
+	})
+	alice.SetGlobalAccountData(t, "m.direct", map[string]interface{}{
+		bob.UserID: []string{roomID},
+	})
+
+	checkAccountData := func(r gjson.Result) bool {
+		if r.Get("type").Str != "m.direct" {
+			return false
+		}
+		content := r.Get("content")
+		rooms := content.Get(bob.UserID)
+		if !rooms.Exists() || !rooms.IsArray() {
+			t.Errorf("m.direct event missing rooms array for user %s", bob.UserID)
+			return false
+		}
+		if rooms.Array()[0].Str != roomID {
+			t.Errorf("m.direct room for %s mismatch: got %v want %v", bob.UserID, rooms.Str, roomID)
+		}
+		return true
+	}
+	since := alice.MustSyncUntil(t, client.SyncReq{}, client.SyncGlobalAccountDataHas(checkAccountData))
+	// now update the DM room and test that incremental syncing also pushes new account data
+	roomID = alice.CreateRoom(t, map[string]interface{}{
+		"invite":    []string{bob.UserID},
+		"is_direct": true,
+	})
+	alice.SetGlobalAccountData(t, "m.direct", map[string]interface{}{
+		bob.UserID: []string{roomID},
+	})
+	alice.MustSyncUntil(t, client.SyncReq{Since: since}, client.SyncGlobalAccountDataHas(checkAccountData))
+
+	// check that manually GETing the account data also works with the new updated value
+	must.MatchResponse(t, alice.GetGlobalAccountData(t, "m.direct"), match.HTTPResponse{
+		StatusCode: 200,
+		JSON: []match.JSON{
+			match.JSONKeyEqual(client.GjsonEscape(bob.UserID), []interface{}{roomID}),
+		},
+	})
+}
+
+// Test that the `is_direct` flag on m.room.member invites propagate to the target user. Both users
+// are on the same homeserver.
 func TestIsDirectFlagLocal(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintOneToOneRoom)
 	defer deployment.Destroy(t)
@@ -44,6 +99,8 @@ func TestIsDirectFlagLocal(t *testing.T) {
 	})
 }
 
+// Test that the `is_direct` flag on m.room.member invites propagate to the target user. Users
+// are on different homeservers.
 func TestIsDirectFlagFederation(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
