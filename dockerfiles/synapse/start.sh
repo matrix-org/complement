f07829e2990eca5c601f484309a77b25acd74c0c
Eric Eastwood <erice@element.io>
2022-02-24 20:19:35 -0600

    Fix "Unrecognised appservice access token" errors
    Regressed in https://github.com/matrix-org/complement/commit/7f19337e4af84bad8bc596c048d70fa19c779b95
    
    In https://github.com/matrix-org/complement/commit/7f19337e4af84bad8bc596c048d70fa19c779b95, the appservice
    mount path changed from `/appservices/` to `/complement/appservice/` but [`start.sh`](https://github.com/matrix-org/complement/blob/53e90f7502ac3c9f1cce7c057ac65562124031d5/dockerfiles/synapse/start.sh#L8)
    which looks at that path was not updated.

--- dockerfiles/synapse/start.sh
+++ dockerfiles/synapse/start.sh
@@ -5,14 +5,14 @@ set -e
 sed -i "s/SERVER_NAME/${SERVER_NAME}/g" /conf/homeserver.yaml
 
 # Add the application service registration files to the homeserver.yaml config
-for filename in /appservices/*.yaml; do
+for filename in /complement/appservice/*.yaml; do
   [ -f "$filename" ] || break
 
   as_id=$(basename "$filename" .yaml)
 
   # Insert the path to the registration file and the AS_REGISTRATION_FILES marker after 
   # so we can add the next application service in the next iteration of this for loop
-  sed -i "s/AS_REGISTRATION_FILES/  - \/appservices\/${as_id}.yaml\nAS_REGISTRATION_FILES/g" /conf/homeserver.yaml
+  sed -i "s/AS_REGISTRATION_FILES/  - \/complement\/appservice\/${as_id}.yaml\nAS_REGISTRATION_FILES/g" /conf/homeserver.yaml
 done
 # Remove the AS_REGISTRATION_FILES entry
 sed -i "s/AS_REGISTRATION_FILES//g" /conf/homeserver.yaml
