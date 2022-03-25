e95787d0b8f7b67507a693ad97ab082c2a343564
Till Faelligen <tfaelligen@gmail.com>
2022-03-25 15:12:05 +0100

    Add topic test

--- tests/csapi/rooms_state_test.go
+++ tests/csapi/rooms_state_test.go
@@ -27,12 +27,11 @@ func TestRoomCreationReportsEventsToMyself(t *testing.T) {
 	userID := "@alice:hs1"
 	alice := deployment.Client(t, "hs1", userID)
 	roomID := alice.CreateRoom(t, struct{}{})
-
+	alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(userID, roomID))
 	t.Run("parallel", func(t *testing.T) {
 		// sytest: Room creation reports m.room.create to myself
 		t.Run("Room creation reports m.room.create to myself", func(t *testing.T) {
 			t.Parallel()
-			alice := deployment.Client(t, "hs1", userID)
 			alice.MustSyncUntil(t, client.SyncReq{}, client.SyncTimelineHas(roomID, func(ev gjson.Result) bool {
 				if ev.Get("type").Str != "m.room.create" {
 					return false
@@ -45,7 +44,6 @@ func TestRoomCreationReportsEventsToMyself(t *testing.T) {
 		// sytest: Room creation reports m.room.member to myself
 		t.Run("Room creation reports m.room.member to myself", func(t *testing.T) {
 			t.Parallel()
-			alice := deployment.Client(t, "hs1", userID)
 			alice.MustSyncUntil(t, client.SyncReq{}, client.SyncTimelineHas(roomID, func(ev gjson.Result) bool {
 				if ev.Get("type").Str != "m.room.member" {
 					return false
@@ -53,6 +51,26 @@ func TestRoomCreationReportsEventsToMyself(t *testing.T) {
 				must.EqualStr(t, ev.Get("sender").Str, userID, "wrong sender")
 				must.EqualStr(t, ev.Get("state_key").Str, userID, "wrong state_key")
 				must.EqualStr(t, ev.Get("content").Get("membership").Str, "join", "wrong content.membership")
+				return true
+			}))
+		})
+		// sytest: Setting room topic reports m.room.topic to myself
+		t.Run("Setting room topic reports m.room.topic to myself", func(t *testing.T) {
+			t.Parallel()
+			topic := "Testing topic for the new room"
+			reqBody := client.WithJSONBody(t, map[string]string{
+				"topic": topic,
+			})
+			alice.MustDoFunc(t, "PUT", []string{"_matrix", "client", "v3", "rooms", roomID, "state", "m.room.topic"}, reqBody)
+			alice.MustSyncUntil(t, client.SyncReq{}, client.SyncTimelineHas(roomID, func(ev gjson.Result) bool {
+				if ev.Get("type").Str != "m.room.topic" {
+					return false
+				}
+				if !ev.Get("sender").Exists() || !ev.Get("content").Exists() {
+					return false
+				}
+				must.EqualStr(t, ev.Get("sender").Str, userID, "wrong sender")
+				must.EqualStr(t, ev.Get("content.topic").Str, topic, "wrong topic")
 				return true
 			}))
 		})
