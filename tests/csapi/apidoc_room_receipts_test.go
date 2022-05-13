cf7114660a8a7fc9eb2a7b5cdafd5d51a2171d11
Till Faelligen <davidf@element.io>
2022-05-13 11:00:20 +0200

    Fix parallel run

--- tests/csapi/apidoc_room_receipts_test.go
+++ tests/csapi/apidoc_room_receipts_test.go
@@ -17,41 +17,43 @@ func TestRoomReceiptsReadMarkers(t *testing.T) {
 	roomID := alice.CreateRoom(t, map[string]interface{}{"preset": "public_chat"})
 
 	// sytest: POST /rooms/:room_id/receipt can create receipts
-	t.Run("POST /rooms/:room_id/receipt can create receipts", func(t *testing.T) {
-		t.Parallel()
-		eventID := ""
-		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncTimelineHas(roomID, func(result gjson.Result) bool {
-			if result.Get("type").Str == "m.room.member" {
-				eventID = result.Get("event_id").Str
-				return true
+	t.Run("Parallel", func(t *testing.T) {
+		t.Run("POST /rooms/:room_id/receipt can create receipts", func(t *testing.T) {
+			t.Parallel()
+			eventID := ""
+			alice.MustSyncUntil(t, client.SyncReq{}, client.SyncTimelineHas(roomID, func(result gjson.Result) bool {
+				if result.Get("type").Str == "m.room.member" {
+					eventID = result.Get("event_id").Str
+					return true
+				}
+				return false
+			}))
+			if eventID == "" {
+				t.Fatal("did not find an event_id")
 			}
-			return false
-		}))
-		if eventID == "" {
-			t.Fatal("did not find an event_id")
-		}
-		alice.MustDoFunc(t, "POST", []string{"_matrix", "client", "v3", "rooms", roomID, "receipt", "m.read", eventID})
-	})
-
-	// sytest: POST /rooms/:room_id/read_markers can create read marker
-	t.Run("POST /rooms/:room_id/read_markers can create read marker", func(t *testing.T) {
-		t.Parallel()
-		eventID := ""
-		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncTimelineHas(roomID, func(result gjson.Result) bool {
-			if result.Get("type").Str == "m.room.member" {
-				eventID = result.Get("event_id").Str
-				return true
-			}
-			return false
-		}))
-		if eventID == "" {
-			t.Fatal("did not find an event_id")
-		}
-
-		reqBody := client.WithJSONBody(t, map[string]interface{}{
-			"m.fully_read": eventID,
-			"m.read":       eventID,
+			alice.MustDoFunc(t, "POST", []string{"_matrix", "client", "v3", "rooms", roomID, "receipt", "m.read", eventID})
 		})
-		alice.MustDoFunc(t, "POST", []string{"_matrix", "client", "v3", "rooms", roomID, "read_markers"}, reqBody)
+
+		// sytest: POST /rooms/:room_id/read_markers can create read marker
+		t.Run("POST /rooms/:room_id/read_markers can create read marker", func(t *testing.T) {
+			t.Parallel()
+			eventID := ""
+			alice.MustSyncUntil(t, client.SyncReq{}, client.SyncTimelineHas(roomID, func(result gjson.Result) bool {
+				if result.Get("type").Str == "m.room.member" {
+					eventID = result.Get("event_id").Str
+					return true
+				}
+				return false
+			}))
+			if eventID == "" {
+				t.Fatal("did not find an event_id")
+			}
+
+			reqBody := client.WithJSONBody(t, map[string]interface{}{
+				"m.fully_read": eventID,
+				"m.read":       eventID,
+			})
+			alice.MustDoFunc(t, "POST", []string{"_matrix", "client", "v3", "rooms", roomID, "read_markers"}, reqBody)
+		})
 	})
 }
