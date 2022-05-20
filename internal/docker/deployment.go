8f3c39d10aca4fd0b758d30fdb50b5cbf9efa2b5
Till Faelligen <davidf@element.io>
2022-05-20 13:35:25 +0200

    Add DeviceID to client.CSAPI

--- internal/docker/deployment.go
+++ internal/docker/deployment.go
@@ -27,6 +27,7 @@ type HomeserverDeployment struct {
 	ContainerID         string            // e.g 10de45efba
 	AccessTokens        map[string]string // e.g { "@alice:hs1": "myAcc3ssT0ken" }
 	ApplicationServices map[string]string // e.g { "my-as-id": "id: xxx\nas_token: xxx ..."} }
+	DeviceIDs           map[string]string // e.g { "@alice:hs1": "myDeviceID" }
 }
 
 // Destroy the entire deployment. Destroys all running containers. If `printServerLogs` is true,
@@ -51,9 +52,15 @@ func (d *Deployment) Client(t *testing.T, hsName, userID string) *client.CSAPI {
 		t.Fatalf("Deployment.Client - HS name '%s' - user ID '%s' not found", hsName, userID)
 		return nil
 	}
+	deviceID := dep.DeviceIDs[userID]
+	if deviceID == "" && userID != "" {
+		t.Fatalf("Deployment.Client - HS name '%s' - user ID '%s' not found", hsName, userID)
+		return nil
+	}
 	return &client.CSAPI{
 		UserID:           userID,
 		AccessToken:      token,
+		DeviceID:         deviceID,
 		BaseURL:          dep.BaseURL,
 		Client:           client.NewLoggedClient(t, hsName, nil),
 		SyncUntilTimeout: 5 * time.Second,
@@ -75,11 +82,11 @@ func (d *Deployment) RegisterUser(t *testing.T, hsName, localpart, password stri
 		SyncUntilTimeout: 5 * time.Second,
 		Debug:            d.Deployer.debugLogging,
 	}
-	var userID, accessToken string
+	var userID, accessToken, deviceID string
 	if isAdmin {
-		userID, accessToken = client.RegisterSharedSecret(t, localpart, password, isAdmin)
+		userID, accessToken, deviceID = client.RegisterSharedSecret(t, localpart, password, isAdmin)
 	} else {
-		userID, accessToken = client.RegisterUser(t, localpart, password)
+		userID, accessToken, deviceID = client.RegisterUser(t, localpart, password)
 	}
 
 	// remember the token so subsequent calls to deployment.Client return the user
@@ -87,5 +94,6 @@ func (d *Deployment) RegisterUser(t *testing.T, hsName, localpart, password stri
 
 	client.UserID = userID
 	client.AccessToken = accessToken
+	client.DeviceID = deviceID
 	return client
 }
