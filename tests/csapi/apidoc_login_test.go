e08054c34b918c150bf893c6ef1989f327e77229
Tommie Gannert <tommie@gannert.se>
2022-05-27 19:27:20 +0200

    Add an OpenID Connect SSO test.
    SyTest only has CAS tests, which is a deprecated login API. The only
    relevant SSO test it has is whether `m.login.sso` is presented as a
    flow. That's added here for completeness:
    
    ./tests/12login/02cas.pl:test "login types include SSO"

--- tests/csapi/apidoc_login_test.go
+++ tests/csapi/apidoc_login_test.go
@@ -3,18 +3,32 @@ package csapi_tests
 import (
 	"encoding/json"
 	"fmt"
+	"net"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strings"
+	"sync"
 	"testing"
 
 	"github.com/tidwall/gjson"
 
 	"github.com/matrix-org/complement/internal/b"
 	"github.com/matrix-org/complement/internal/client"
+	"github.com/matrix-org/complement/internal/docker"
 	"github.com/matrix-org/complement/internal/match"
 	"github.com/matrix-org/complement/internal/must"
 )
 
 func TestLogin(t *testing.T) {
-	deployment := Deploy(t, b.BlueprintAlice)
+	idpMux := http.NewServeMux()
+	idpURL, idpStop, err := newFakeHTTPServer(idpMux)
+	if err != nil {
+		t.Fatalf("newFakeHTTPServer failed: %v", err)
+	}
+	defer idpStop()
+
+	deployment := Deploy(t, b.BlueprintAlice, WithEnv("FAKE_SSO_URL="+idpURL.String()))
 	defer deployment.Destroy(t)
 	unauthedClient := deployment.Client(t, "hs1", "")
 	_ = deployment.RegisterUser(t, "hs1", "test_login_user", "superuser", false)
@@ -134,6 +148,136 @@ func TestLogin(t *testing.T) {
 			})
 		})
 
+		// sytest: login types include SSO
+		t.Run("login types include SSO", func(t *testing.T) {
+			t.Parallel()
+			res := unauthedClient.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "login"}, client.WithRawBody(json.RawMessage(`{}`)))
+			must.MatchResponse(t, res, match.HTTPResponse{
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+				},
+				JSON: []match.JSON{
+					match.JSONArraySome("flows", func(val gjson.Result) error {
+						if val.Get("type").String() != "m.login.sso" {
+							return fmt.Errorf("expected some flow to have an 'm.login.sso' type")
+						}
+						return nil
+					}),
+				},
+			})
+		})
+		t.Run("can log in using SSO with OIDC", func(t *testing.T) {
+			t.Parallel()
+			idpMux.Handle("/.well-known/openid-configuration", jsonHandlerFunc(func(w http.ResponseWriter, r *http.Request) (interface{}, error) {
+				return struct {
+					Issuer                string `json:"issuer"`
+					AuthorizationEndpoint string `json:"authorization_endpoint"`
+					TokenEndpoint         string `json:"token_endpoint"`
+					UserinfoEndpoint      string `json:"userinfo_endpoint"`
+				}{
+					Issuer:                "http://issuer.example.com/",
+					AuthorizationEndpoint: idpURL.String() + "authorize",
+					TokenEndpoint:         idpURL.String() + "token",
+					UserinfoEndpoint:      idpURL.String() + "userinfo",
+				}, nil
+			}))
+			hc := *unauthedClient.Client
+			hc.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			}
+			var err error
+			hc.Jar, err = cookiejar.New(nil)
+			if err != nil {
+				t.Fatalf("cookiejar.New failed: %v", err)
+			}
+			cclient := *unauthedClient
+			cclient.Client = &hc
+			res := cclient.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "login", "sso", "redirect", "google"}, client.WithQueries(url.Values{
+				"redirectUrl": []string{"http://example.com/continue"},
+			}))
+			must.MatchResponse(t, res, match.HTTPResponse{
+				StatusCode: 302,
+				HeadersRE: map[string]string{
+					"Location": idpURL.String() + `authorize\?client_id=aclientid&redirect_uri=http%3A%2F%2Flocalhost.*%2F_matrix%2Fclient%2Fr0%2Flogin%2Fsso%2Fcallback%3Fprovider%3Dgoogle&response_type=code&scope=openid\+profile\+email&state=.*`,
+				},
+			})
+
+			authURL, err := url.Parse(res.Header.Get("Location"))
+			if err != nil {
+				t.Fatalf("url.Parse failed: %v", err)
+			}
+
+			idpMux.Handle("/token", jsonHandlerFunc(func(w http.ResponseWriter, r *http.Request) (interface{}, error) {
+				if got, want := r.FormValue("code"), "acode"; got != want {
+					return nil, fmt.Errorf("code: got %q, want %q", got, want)
+				}
+				if got, want := r.FormValue("client_id"), "aclientid"; got != want {
+					return nil, fmt.Errorf("client_id: got %q, want %q", got, want)
+				}
+				if got, want := r.FormValue("client_secret"), "aclientsecret"; got != want {
+					return nil, fmt.Errorf("client_secret: got %q, want %q", got, want)
+				}
+				if got, want := r.FormValue("redirect_uri"), "/login/sso/callback?provider=google"; !strings.HasSuffix(got, want) {
+					return nil, fmt.Errorf("redirect_uri: got %q, want suffix %q", got, want)
+				}
+				if got, want := r.FormValue("grant_type"), "authorization_code"; got != want {
+					return nil, fmt.Errorf("grant_type: got %q, want %q", got, want)
+				}
+
+				return struct {
+					AccessToken string `json:"access_token"`
+					TokenType   string `json:"token_type"`
+				}{
+					AccessToken: "anaccesstoken",
+					TokenType:   "bearer",
+				}, nil
+			}))
+			idpMux.Handle("/userinfo", jsonHandlerFunc(func(w http.ResponseWriter, r *http.Request) (interface{}, error) {
+				if got, want := r.Header.Get("Authorization"), "Bearer anaccesstoken"; got != want {
+					return nil, fmt.Errorf("Authorization header: got %q, want %q", got, want)
+				}
+				if got, want := r.Header.Get("Accept"), "application/json"; got != want {
+					return nil, fmt.Errorf("Accept header: got %q, want %q", got, want)
+				}
+
+				return struct {
+					Sub  string `json:"sub"`
+					Name string `json:"name"`
+				}{
+					Sub:  "asubject",
+					Name: "A Name",
+				}, nil
+			}))
+
+			res = cclient.DoFunc(t, "GET", []string{"_matrix", "client", "r0", "login", "sso", "callback"}, client.WithQueries(url.Values{
+				"provider": []string{"google"},
+				"code":     []string{"acode"},
+				"state":    authURL.Query()["state"],
+			}))
+			must.MatchResponse(t, res, match.HTTPResponse{
+				StatusCode: 302,
+				HeadersRE: map[string]string{
+					"Location": `http://example.com/continue\?loginToken=.+`,
+				},
+			})
+
+			loginURL, err := url.Parse(res.Header.Get("Location"))
+			if err != nil {
+				t.Fatalf("url.Parse failed: %v", err)
+			}
+
+			res = unauthedClient.MustDoFunc(t, "POST", []string{"_matrix", "client", "r0", "login"}, client.WithRawBody(json.RawMessage(fmt.Sprintf(`{
+				"type": "m.login.token",
+				"token": "%s"
+			}`, loginURL.Query().Get("loginToken")))))
+			body := must.ParseJSON(t, res.Body)
+			defer res.Body.Close()
+			js := gjson.ParseBytes(body)
+			unauthedClient.UserID = js.Get("user_id").Str
+			unauthedClient.AccessToken = js.Get("access_token").Str
+			unauthedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "account", "whoami"})
+		})
+
 		// Regression test for https://github.com/matrix-org/dendrite/issues/2287
 		t.Run("Login with uppercase username works and GET /whoami afterwards also", func(t *testing.T) {
 			t.Parallel()
@@ -155,5 +299,51 @@ func TestLogin(t *testing.T) {
 			// check that we can successfully query /whoami
 			unauthedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "r0", "account", "whoami"})
 		})
+	})
+}
+
+func newFakeHTTPServer(h http.Handler) (baseURL *url.URL, stop func(), _ error) {
+	const dockerInterfaceAddr = ""
+	l, err := net.Listen("tcp", dockerInterfaceAddr+":0")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	s := http.Server{
+		Addr:    l.Addr().String(),
+		Handler: h,
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		s.Serve(l)
+	}()
+
+	return &url.URL{
+			Scheme: "http",
+			Host:   fmt.Sprint(docker.HostnameRunningComplement, ":", l.Addr().(*net.TCPAddr).Port),
+			Path:   "/",
+		}, func() {
+			s.Close()
+			wg.Wait()
+		}, nil
+}
+
+func jsonHandlerFunc(fun func(http.ResponseWriter, *http.Request) (interface{}, error)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp, err := fun(w, r)
+
+		if err != nil {
+			w.Header().Set("Content-Type", "text/plain")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			w.Header().Set("Content-Type", "text/plain")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 }
