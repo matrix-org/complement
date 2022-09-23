e99b6c69eace7cc1faade4c74c6a7b558001f902
Sean Quah <seanq@matrix.org>
2022-09-23 18:14:32 +0100

    Add `SendJoinRequestsHandler` option to omit other servers in room

--- tests/federation_room_join_test.go
+++ tests/federation_room_join_test.go
@@ -62,7 +62,7 @@ func TestJoinViaRoomIDAndServerName(t *testing.T) {
 			w.WriteHeader(502)
 			return
 		}
-		federation.SendJoinRequestsHandler(srv, w, req, false)
+		federation.SendJoinRequestsHandler(srv, w, req, false, false)
 	})).Methods("PUT")
 
 	ver := alice.GetDefaultRoomVersion(t)
