1211333be517f745d325a9508372ee53461f52c2
Neil Alexander <neilalexander@users.noreply.github.com>
2022-10-12 09:44:03 +0100

    Update room_relations_test.go

--- tests/csapi/room_relations_test.go
+++ tests/csapi/room_relations_test.go
@@ -16,7 +16,6 @@ import (
 )
 
 func TestRelations(t *testing.T) {
-	runtime.SkipIf(t, runtime.Dendrite) // not supported
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
 
@@ -112,7 +111,6 @@ func TestRelations(t *testing.T) {
 }
 
 func TestRelationsPagination(t *testing.T) {
-	runtime.SkipIf(t, runtime.Dendrite) // not supported
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
 
