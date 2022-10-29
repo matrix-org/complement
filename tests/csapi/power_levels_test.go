96fedf75128e974ddebdf2ca0ceea346163fee1e
Jonathan de Jong <jonathan@automatia.nl>
2022-10-29 14:16:52 +0200

    add 10apidoc/36room-levels

--- tests/csapi/power_levels_test.go
+++ tests/csapi/power_levels_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 
 	"github.com/matrix-org/complement/internal/b"
+	"github.com/matrix-org/complement/internal/client"
+	"github.com/matrix-org/complement/internal/match"
+	"github.com/matrix-org/complement/internal/must"
 )
 
 // This test ensures that an authorised (PL 100) user is able to modify the users_default value
@@ -38,5 +41,85 @@ func TestDemotingUsersViaUsersDefault(t *testing.T) {
 			"events":        map[string]int64{},
 			"notifications": map[string]int64{},
 		},
+	})
+}
+
+func TestPowerLevels(t *testing.T) {
+	deployment := Deploy(t, b.BlueprintAlice)
+	defer deployment.Destroy(t)
+
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+
+	roomID := alice.CreateRoom(t, map[string]interface{}{})
+
+	// sytest: GET /rooms/:room_id/state/m.room.power_levels can fetch levels
+	t.Run("GET /rooms/:room_id/state/m.room.power_levels can fetch levels", func(t *testing.T) {
+		// Test if the old state still exists
+		res := alice.MustDoFunc(t, "GET", []string{"_matrix", "client", "v3", "rooms", roomID, "state", "m.room.power_levels"})
+
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+			JSON: []match.JSON{
+				match.JSONKeyPresent("users"),
+			},
+		})
+	})
+
+	// sytest: PUT /rooms/:room_id/state/m.room.power_levels can set levels
+	t.Run("PUT /rooms/:room_id/state/m.room.power_levels can set levels", func(t *testing.T) {
+		alice.SendEventSynced(t, roomID, b.Event{
+			Type:     "m.room.power_levels",
+			StateKey: b.Ptr(""),
+			Content: map[string]interface{}{
+				"invite": 100,
+				"users": map[string]interface{}{
+					alice.UserID: 100,
+				},
+			},
+		})
+	})
+
+	// sytest: PUT power_levels should not explode if the old power levels were empty
+	t.Run("PUT power_levels should not explode if the old power levels were empty", func(t *testing.T) {
+		// Absence of an "events" key
+		alice.SendEventSynced(t, roomID, b.Event{
+			Type:     "m.room.power_levels",
+			StateKey: b.Ptr(""),
+			Content: map[string]interface{}{
+				"users": map[string]interface{}{
+					alice.UserID: 100,
+				},
+			},
+		})
+
+		// Absence of a "users" key
+		alice.SendEventSynced(t, roomID, b.Event{
+			Type:     "m.room.power_levels",
+			StateKey: b.Ptr(""),
+			Content:  map[string]interface{}{},
+		})
+
+		// This should give a 403 (not a 500)
+		res := alice.DoFunc(
+			t,
+			"PUT",
+			[]string{"_matrix", "client", "v3", "rooms", roomID, "state", "m.room.power_levels"},
+			client.WithJSONBody(t, map[string]interface{}{
+				"users": map[string]string{},
+			}),
+		)
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 403,
+		})
+
+		// Test if the old state still exists
+		res = alice.MustDoFunc(t, "GET", []string{"_matrix", "client", "v3", "rooms", roomID, "state", "m.room.power_levels"})
+
+		must.MatchResponse(t, res, match.HTTPResponse{
+			StatusCode: 200,
+			JSON: []match.JSON{
+				match.JSONKeyMissing("users"),
+			},
+		})
 	})
 }
