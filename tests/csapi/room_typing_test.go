f0c32367575306728685c82d03a624fb3c104921
Jonathan de Jong <jonathan@automatia.nl>
2022-10-31 15:36:59 +0100

    Sytest: Add `90jira/SYN-328` test (#535)

--- tests/csapi/room_typing_test.go
+++ tests/csapi/room_typing_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/matrix-org/complement/internal/b"
 	"github.com/matrix-org/complement/internal/client"
+	"github.com/matrix-org/complement/internal/match"
 	"github.com/matrix-org/complement/internal/must"
 )
 
@@ -68,4 +69,61 @@ func TestTyping(t *testing.T) {
 			return len(result.Get("content").Get("user_ids").Array()) == 0
 		}))
 	})
+}
+
+// sytest: Typing notifications don't leak
+func TestLeakyTyping(t *testing.T) {
+	deployment := Deploy(t, b.BlueprintOneToOneRoom)
+	defer deployment.Destroy(t)
+
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+	bob := deployment.Client(t, "hs1", "@bob:hs1")
+	charlie := deployment.RegisterUser(t, "hs1", "charlie", "charliepassword", false)
+
+	// Alice creates a room. Bob joins it.
+	roomID := alice.CreateRoom(t, map[string]interface{}{"preset": "public_chat"})
+	bob.JoinRoom(t, roomID, nil)
+
+	bobToken := bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(bob.UserID, roomID))
+
+	_, charlieToken := charlie.MustSync(t, client.SyncReq{TimeoutMillis: "0"})
+
+	// Alice types in that room. Bob should see her typing.
+	alice.MustDoFunc(t, "PUT", []string{"_matrix", "client", "v3", "rooms", roomID, "typing", alice.UserID}, client.WithJSONBody(t, map[string]interface{}{
+		"typing":  true,
+		"timeout": 10000,
+	}))
+
+	bob.MustSyncUntil(t, client.SyncReq{Since: bobToken}, client.SyncEphemeralHas(roomID, func(result gjson.Result) bool {
+		if result.Get("type").Str != "m.typing" {
+			return false
+		}
+
+		err := match.JSONCheckOff("content.user_ids", []interface{}{
+			alice.UserID,
+		}, func(result gjson.Result) interface{} {
+			return result.Str
+		}, nil)([]byte(result.Raw))
+
+		return err == nil
+	}))
+
+	// Charlie is not in the room, so should not see Alice typing.
+	res, _ := charlie.MustSync(t, client.SyncReq{TimeoutMillis: "2000", Since: charlieToken})
+
+	err := client.SyncEphemeralHas(roomID, func(result gjson.Result) bool {
+		if result.Get("type").Str != "m.typing" {
+			return false
+		}
+		for _, item := range result.Get("content").Get("user_ids").Array() {
+			if item.Str == charlie.UserID {
+				return true
+			}
+		}
+		return false
+	})(charlie.UserID, res)
+
+	if err == nil {
+		t.Fatalf("Received unexpected typing notification: %s", res.Raw)
+	}
 }
