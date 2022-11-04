7c95cc575891e162d648a59f0315054e7eae2dd5
Till Faelligen <2353100+S7evinK@users.noreply.github.com>
2022-11-04 15:12:32 +0100

    Add regression test for room summaries

--- tests/csapi/sync_test.go
+++ tests/csapi/sync_test.go
@@ -435,6 +435,50 @@ func TestPresenceSyncDifferentRooms(t *testing.T) {
 	})
 }
 
+func TestRoomSummary(t *testing.T) {
+	deployment := Deploy(t, b.BlueprintOneToOneRoom)
+	defer deployment.Destroy(t)
+	alice := deployment.Client(t, "hs1", "@alice:hs1")
+	bob := deployment.Client(t, "hs1", "@bob:hs1")
+
+	_, aliceSince := alice.MustSync(t, client.SyncReq{TimeoutMillis: "0"})
+	roomID := alice.CreateRoom(t, map[string]interface{}{
+		"preset": "public_chat",
+		"invite": []string{bob.UserID},
+	})
+	aliceSince = alice.MustSyncUntil(t, client.SyncReq{Since: aliceSince},
+		client.SyncJoinedTo(alice.UserID, roomID),
+		func(clientUserID string, syncResp gjson.Result) error {
+			summary := syncResp.Get("rooms.join." + client.GjsonEscape(roomID) + ".summary")
+			invitedUsers := summary.Get(client.GjsonEscape("m.invited_member_count")).Int()
+			joinedUsers := summary.Get(client.GjsonEscape("m.joined_member_count")).Int()
+			// We expect there to be one joined and one invited user
+			if invitedUsers != 1 || joinedUsers != 1 {
+				return fmt.Errorf("expected one invited and one joined user, got %d and %d: %v", invitedUsers, joinedUsers, summary.Raw)
+			}
+			return nil
+		},
+	)
+
+	joinedCheck := func(clientUserID string, syncResp gjson.Result) error {
+		summary := syncResp.Get("rooms.join." + client.GjsonEscape(roomID) + ".summary")
+		invitedUsers := summary.Get(client.GjsonEscape("m.invited_member_count")).Int()
+		joinedUsers := summary.Get(client.GjsonEscape("m.joined_member_count")).Int()
+		// We expect there to be two joined and no invited user
+		if invitedUsers != 0 || joinedUsers != 2 {
+			return fmt.Errorf("expected no invited and two joined user, got %d and %d: %v", invitedUsers, joinedUsers, summary.Raw)
+		}
+		return nil
+	}
+
+	sinceToken := bob.MustSyncUntil(t, client.SyncReq{}, client.SyncInvitedTo(bob.UserID, roomID))
+	bob.JoinRoom(t, roomID, []string{})
+	// Verify Bob sees the correct room summary
+	bob.MustSyncUntil(t, client.SyncReq{Since: sinceToken}, client.SyncJoinedTo(bob.UserID, roomID), joinedCheck)
+	// .. and Alice as well.
+	alice.MustSyncUntil(t, client.SyncReq{Since: aliceSince}, client.SyncJoinedTo(bob.UserID, roomID), joinedCheck)
+}
+
 func sendMessages(t *testing.T, client *client.CSAPI, roomID string, prefix string, count int) {
 	t.Helper()
 	for i := 0; i < count; i++ {
