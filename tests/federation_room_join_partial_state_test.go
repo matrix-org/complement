dbfed4ea8f26a431600b0efe1e53b24b319a99c8
David Robertson <davidr@element.io>
2023-02-13 12:43:50 +0000

    Unskip leave-during-resync test

--- tests/federation_room_join_partial_state_test.go
+++ tests/federation_room_join_partial_state_test.go
@@ -3460,7 +3460,6 @@ func TestPartialStateJoin(t *testing.T) {
 	})
 
 	t.Run("Leaving a room immediately after joining does not wait for resync", func(t *testing.T) {
-		t.Skip("Not yet implemented (synapse#12802)")
 		// Prepare to listen for leave events from the HS under test.
 		// We're only expecting one leave event, but give the channel extra capacity
 		// to avoid deadlock if the HS does something silly.
