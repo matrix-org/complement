eb6fbd8cc5f0b2968ceb534d386454353b74ff1c
Kegan Dougal <kegan@matrix.org>
2023-10-12 11:58:49 +0100

    Remove Deploy proxy function as it is useless

--- tests/csapi/main_test.go
+++ tests/csapi/main_test.go
@@ -4,15 +4,8 @@ import (
 	"testing"
 
 	"github.com/matrix-org/complement"
-	"github.com/matrix-org/complement/b"
-	"github.com/matrix-org/complement/internal/docker"
 )
 
 func TestMain(m *testing.M) {
 	complement.TestMain(m, "csapi")
-}
-
-func Deploy(t *testing.T, blueprint b.Blueprint) *docker.Deployment {
-	t.Helper()
-	return complement.Deploy(t, blueprint)
 }
--- tests/msc3890/main_test.go
+++ tests/msc3890/main_test.go
@@ -4,15 +4,8 @@ import (
 	"testing"
 
 	"github.com/matrix-org/complement"
-	"github.com/matrix-org/complement/b"
-	"github.com/matrix-org/complement/internal/docker"
 )
 
 func TestMain(m *testing.M) {
 	complement.TestMain(m, "msc3890")
-}
-
-func Deploy(t *testing.T, blueprint b.Blueprint) *docker.Deployment {
-	t.Helper()
-	return complement.Deploy(t, blueprint)
 }
