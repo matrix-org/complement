94a8e6117c71c765f500af86d48f056224ec2bd8
Eric Eastwood <erice@element.io>
2025-05-12 12:05:13 -0500

    Add args to custom deployment/cleanup

--- test_main.go
+++ test_main.go
@@ -6,17 +6,18 @@ import (
 	"testing"
 
 	"github.com/matrix-org/complement/b"
+	"github.com/matrix-org/complement/config"
 	"github.com/matrix-org/complement/ct"
 )
 
 var (
 	testPackage    *TestPackage
-	customDeployer func(numServers int) Deployment
+	customDeployer func(t ct.TestLike, numServers int, config *config.Complement) Deployment
 )
 
 type complementOpts struct {
-	cleanup          func()
-	customDeployment func(numServers int) Deployment
+	cleanup          func(config *config.Complement)
+	customDeployment func(t ct.TestLike, numServers int, config *config.Complement) Deployment
 }
 type opt func(*complementOpts)
 
@@ -24,14 +25,14 @@ type opt func(*complementOpts)
 // It is called BEFORE Complement containers are destroyed.
 // This function should be used for per-suite cleanup operations e.g tearing down containers, killing
 // child processes, etc.
-func WithCleanup(fn func()) opt {
+func WithCleanup(fn func(config *config.Complement)) opt {
 	return func(co *complementOpts) {
 		co.cleanup = fn
 	}
 }
 
 // WithDeployment adds a custom mechanism to deploy homeservers.
-func WithDeployment(fn func(numServers int) Deployment) opt {
+func WithDeployment(fn func(t ct.TestLike, numServers int, config *config.Complement) Deployment) opt {
 	return func(co *complementOpts) {
 		co.customDeployment = fn
 	}
@@ -64,7 +65,8 @@ func TestMain(m *testing.M, namespace string, customOpts ...opt) {
 	}
 	exitCode := m.Run()
 	if opts.cleanup != nil {
-		opts.cleanup()
+		// TODO: Need access to `testPackage.Config` to see if `PackageNamespace` etc.
+		opts.cleanup(testPackage.Config)
 	}
 	testPackage.Cleanup()
 	os.Exit(exitCode)
@@ -91,7 +93,9 @@ func Deploy(t ct.TestLike, numServers int) Deployment {
 		ct.Fatalf(t, "Deploy: testPackage not set, did you forget to call complement.TestMain?")
 	}
 	if customDeployer != nil {
-		return customDeployer(numServers)
+		// TODO: Need a way to return an error or pass in `t`
+		// TODO: Need access to `testPackage.Config` to see if `DebugLoggingEnabled`, `SpawnHSTimeout`, `PackageNamespace` etc.
+		return customDeployer(t, numServers, testPackage.Config)
 	}
 	return testPackage.Deploy(t, numServers)
 }
