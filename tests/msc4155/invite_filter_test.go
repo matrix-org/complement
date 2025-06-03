684b12cc86edb6303d9c927e15d6a703bc116c00
Half-Shot <will@half-shot.uk>
2025-06-03 12:23:04 +0100

    update tests

--- tests/msc4155/invite_filter_test.go
+++ tests/msc4155/invite_filter_test.go
@@ -1,16 +1,16 @@
 package tests
 
 import (
-	"testing"
-	"github.com/matrix-org/complement"
-	"github.com/matrix-org/complement/helpers"
-	"github.com/matrix-org/complement/must"
-	"github.com/matrix-org/complement/match"
-	"github.com/matrix-org/gomatrixserverlib/spec"
-	"github.com/matrix-org/complement/client"
 	"encoding/json"
+	"github.com/matrix-org/complement"
+	"github.com/matrix-org/complement/client"
 	"github.com/matrix-org/complement/ct"
+	"github.com/matrix-org/complement/helpers"
+	"github.com/matrix-org/complement/match"
+	"github.com/matrix-org/complement/must"
+	"github.com/matrix-org/gomatrixserverlib/spec"
 	"io"
+	"testing"
 )
 
 const hs1Name = "hs1"
@@ -32,23 +32,23 @@ func TestInviteFiltering(t *testing.T) {
 	defer deployment.Destroy(t)
 
 	// Invitee
-	alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 	evil_alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 	
 	bob := deployment.Register(t, hs2Name, helpers.RegistrationOpts{})
 	evil_bob := deployment.Register(t, hs2Name, helpers.RegistrationOpts{})
 
 	t.Run("Can invite users normally without any rules", func(t *testing.T) {
+		// Use a fresh alice each time to ensure we don't have any test cross-contaimination.
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{})
 		roomID := mustCreateRoomAndSync(t, bob)
-		bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(bob.UserID, roomID))
 		bob.MustInviteRoom(t, roomID, alice.UserID)
-		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncInvitedTo(alice.UserID, roomID))
 		alice.MustJoinRoom(t, roomID, []spec.ServerName{})
 		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomID))
 		bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomID))
 	})
 	t.Run("Can block a single user", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{
 			BlockedUsers: []string{bob.UserID},
 		})
@@ -57,6 +57,7 @@ func TestInviteFiltering(t *testing.T) {
 		mustHaveNoInviteInSyncResponse(t, alice)
 	})
 	t.Run("Can ignore a single user", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{
 			IgnoredUsers: []string{bob.UserID},
 		})
@@ -66,6 +67,7 @@ func TestInviteFiltering(t *testing.T) {
 		mustHaveNoInviteInSyncResponse(t, alice)
 	})
 	t.Run("Can block a whole server", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{
 			BlockedServers: []string{hs2Name},
 		})
@@ -73,9 +75,9 @@ func TestInviteFiltering(t *testing.T) {
 		mustInviteRoomAndFail(t, bob, roomIDA, alice.UserID)
 		roomIDB := mustCreateRoomAndSync(t, evil_bob)
 		mustInviteRoomAndFail(t, evil_bob, roomIDB, alice.UserID)
-		mustHaveNoInviteInSyncResponse(t, alice)
 	})
 	t.Run("Can ignore a whole server", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{
 			IgnoredServers: []string{hs2Name},
 		})
@@ -86,6 +88,7 @@ func TestInviteFiltering(t *testing.T) {
 		mustHaveNoInviteInSyncResponse(t, alice)
 	})
 	t.Run("Can glob serveral servers", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{
 			BlockedServers: []string{"hs*"},
 		})
@@ -100,7 +103,27 @@ func TestInviteFiltering(t *testing.T) {
 
 		mustHaveNoInviteInSyncResponse(t, alice)
 	})
+	t.Run("Can glob serveral users", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
+		mustSetInviteConfig(t, alice, InviteFilterConfig{
+			// Test glob behaviours
+			BlockedUsers: []string{
+				"@user-?*",
+			},
+		})
+		roomIDA := mustCreateRoomAndSync(t, bob)
+		mustInviteRoomAndFail(t, bob, roomIDA, alice.UserID)
+		roomIDB := mustCreateRoomAndSync(t, evil_bob)
+		mustInviteRoomAndFail(t, evil_bob, roomIDB, alice.UserID)
+		roomIDC := evil_alice.MustCreateRoom(t, map[string]interface{}{
+			"preset": "private_chat",
+		})
+		mustInviteRoomAndFail(t, evil_alice, roomIDC, alice.UserID)
+
+		mustHaveNoInviteInSyncResponse(t, alice)
+	})
 	t.Run("Can allow a user from a blocked server", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{
 			AllowedUsers: []string{bob.UserID},
 			BlockedServers: []string{hs2Name},
@@ -117,6 +140,7 @@ func TestInviteFiltering(t *testing.T) {
 		bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDAllowed))
 	})
 	t.Run("Can block a user from an allowed server", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
 		mustSetInviteConfig(t, alice, InviteFilterConfig{
 			BlockedUsers: []string{evil_bob.UserID},
 			AllowedServers: []string{hs2Name},
@@ -132,10 +156,50 @@ func TestInviteFiltering(t *testing.T) {
 		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDAllowed))
 		bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDAllowed))
 	})
+	t.Run("Will ignore null fields", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
+		alice.MustSetGlobalAccountData(t, inviteFilterAccountData, map[string]interface{}{
+			"allowed_users": nil,
+			"ignored_users": nil,
+			"blocked_users": nil,
+			"allowed_servers": nil,
+			"ignored_servers": nil,
+			"blocked_servers": nil,
+		})
+		roomIDAllowed := mustCreateRoomAndSync(t, bob)
+		bob.MustInviteRoom(t, roomIDAllowed, alice.UserID)
+		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncInvitedTo(alice.UserID, roomIDAllowed))
+		alice.MustJoinRoom(t, roomIDAllowed, []spec.ServerName{})
+		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDAllowed))
+		bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDAllowed))
+	})
+	t.Run("Will allow users when a user appears in multiple fields", func(t *testing.T) {
+		alice := deployment.Register(t, hs1Name, helpers.RegistrationOpts{})
+		mustSetInviteConfig(t, alice, InviteFilterConfig{
+			IgnoredUsers: []string{bob.UserID},
+			BlockedUsers: []string{evil_bob.UserID},
+			AllowedUsers: []string{bob.UserID, evil_bob.UserID},
+		})
+		roomIDBob := mustCreateRoomAndSync(t, bob)
+		bob.MustInviteRoom(t, roomIDBob, alice.UserID)
+		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncInvitedTo(alice.UserID, roomIDBob))
+		alice.MustJoinRoom(t, roomIDBob, []spec.ServerName{})
+		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDBob))
+		bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDBob))
+
+		roomIDEvilBob := mustCreateRoomAndSync(t, evil_bob)
+		evil_bob.MustInviteRoom(t, roomIDEvilBob, alice.UserID)
+		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncInvitedTo(alice.UserID, roomIDEvilBob))
+		alice.MustJoinRoom(t, roomIDEvilBob, []spec.ServerName{})
+		alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDEvilBob))
+		evil_bob.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(alice.UserID, roomIDEvilBob))
+	})
 }
 
 // Tests that a given invite filter config is properly set
 func mustSetInviteConfig(t *testing.T, c *client.CSAPI, cfg InviteFilterConfig) {
+	// This marshalling section transforms the InviteFilterConfig struct into JSON
+	// we can send to the server.
 	b, err := json.Marshal(&cfg)
 	if err != nil {
 		panic(err)
@@ -145,6 +209,7 @@ func mustSetInviteConfig(t *testing.T, c *client.CSAPI, cfg InviteFilterConfig) 
 	if err != nil {
 		panic(err)
 	}
+
 	c.MustSetGlobalAccountData(t, inviteFilterAccountData, m)
 }
 
