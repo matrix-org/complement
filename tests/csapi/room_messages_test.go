85791c3653660602e1e81fc566717cd128468a25
Eric Eastwood <erice@element.io>
2025-11-13 14:46:19 -0600

    Remove `awaitPartialStateJoinCompletion`
    See https://github.com/matrix-org/complement/pull/816#discussion_r2520215649

--- tests/csapi/room_messages_test.go
+++ tests/csapi/room_messages_test.go
@@ -367,7 +367,6 @@ func TestMessagesOverFederation(t *testing.T) {
 				bob.MustJoinRoom(t, roomID, []spec.ServerName{
 					deployment.GetFullyQualifiedHomeserverName(t, "hs1"),
 				})
-				awaitPartialStateJoinCompletion(t, roomID, bob)
 				aliceSince = alice.MustSyncUntil(t, client.SyncReq{Since: aliceSince}, client.SyncJoinedTo(bob.UserID, roomID))
 
 				// Bob leaves the room
@@ -415,7 +414,6 @@ func _sendAndTestMessageHistory(
 	bob.MustJoinRoom(t, roomID, []spec.ServerName{
 		serverToJoinVia,
 	})
-	awaitPartialStateJoinCompletion(t, roomID, bob)
 
 	// Make it easy to cross-reference the events being talked about in the logs
 	for eventIndex, eventID := range eventIDs {
@@ -628,21 +626,4 @@ func assertMessagesInTimelineInOrder(t *testing.T, actualEventIDs []string, expe
 			)
 		}
 	}
-}
-
-// awaitPartialStateJoinCompletion waits until the joined room is no longer partial-stated
-func awaitPartialStateJoinCompletion(
-	t *testing.T, room_id string, user *client.CSAPI,
-) {
-	t.Helper()
-
-	// Use a `/members` request to wait for the room to be un-partial stated.
-	// We avoid using `/sync`, as it only waits (or used to wait) for full state at
-	// particular events, rather than the whole room.
-	user.MustDo(
-		t,
-		"GET",
-		[]string{"_matrix", "client", "v3", "rooms", room_id, "members"},
-	)
-	t.Logf("%s's partial state join to %s completed.", user.UserID, room_id)
 }
