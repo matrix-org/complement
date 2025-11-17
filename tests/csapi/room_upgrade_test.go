d53a3079e0f7186e2b5aa940ab2d244c0577f48e
Eric Eastwood <erice@element.io>
2025-11-17 16:06:13 -0600

    Test multiple users

--- tests/csapi/room_upgrade_test.go
+++ tests/csapi/room_upgrade_test.go
@@ -17,17 +17,24 @@ func TestPushRuleRoomUpgrade(t *testing.T) {
 	defer deployment.Destroy(t)
 
 	alice := deployment.Register(t, "hs1", helpers.RegistrationOpts{})
+	alice2 := deployment.Register(t, "hs1", helpers.RegistrationOpts{})
 	bob := deployment.Register(t, "hs2", helpers.RegistrationOpts{})
+	bob2 := deployment.Register(t, "hs2", helpers.RegistrationOpts{})
 
 	// When a homeserver becomes aware of a room upgrade (upgrade is done on local
-	// homeserver), it should copy over any push rules for all of its local users from the
-	// old room to the new room.
-	t.Run("upgrading a room carries over the push rules for local users", func(t *testing.T) {
+	// homeserver), it should copy over any existing push rules for all of its local users
+	// from the old room to the new room at the time of upgrade.
+	t.Run("upgrading a room carries over existing push rules for local users", func(t *testing.T) {
 		// Create a room
 		roomID := alice.MustCreateRoom(t, map[string]interface{}{
 			"preset":       "public_chat",
 			"room_version": "10",
 		})
+		// Have alice2 join the room
+		//
+		// We use two users to ensure that all users get taken care of (not just the person
+		// upgrading the room).
+		alice2.MustJoinRoom(t, roomID, nil)
 
 		// Add some push rules
 		alice.MustDo(t, "PUT", []string{"_matrix", "client", "v3", "pushrules", "global", "room", roomID},
@@ -35,41 +42,52 @@ func TestPushRuleRoomUpgrade(t *testing.T) {
 				"actions": []string{"dont_notify"},
 			}),
 		)
+		alice2.MustDo(t, "PUT", []string{"_matrix", "client", "v3", "pushrules", "global", "room", roomID},
+			client.WithJSONBody(t, map[string]interface{}{
+				"actions": []string{"dont_notify"},
+			}),
+		)
 
 		// Sanity check the push rules are in the expected state before the upgrade
-		pushRulesBefore := alice.GetAllPushRules(t)
-		must.MatchGJSON(t, pushRulesBefore,
-			match.JSONCheckOff("global.room", []interface{}{
-				roomID,
-			},
-				match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
-				match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
-					return match.JSONKeyEqual("actions.0", "dont_notify")(result)
-				}),
-			),
-		)
+		for _, client := range []*client.CSAPI{alice, alice2} {
+			pushRulesBefore := client.GetAllPushRules(t)
+			must.MatchGJSON(t, pushRulesBefore,
+				match.JSONCheckOff("global.room", []interface{}{
+					roomID,
+				},
+					match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
+					match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
+						return match.JSONKeyEqual("actions.0", "dont_notify")(result)
+					}),
+				),
+			)
+		}
 
 		// Upgrade the room
 		newRoomID := alice.MustUpgradeRoom(t, roomID, "11")
+		// Alice2 joins the new room
+		alice2.MustJoinRoom(t, newRoomID, nil)
 
 		// Sanity check the push rules are in the expected state before the upgrade
-		pushRulesAfter := alice.GetAllPushRules(t)
-		must.MatchGJSON(t, pushRulesAfter,
-			match.JSONCheckOff("global.room", []interface{}{
-				roomID,
-				newRoomID,
-			},
-				match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
-				match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
-					return match.JSONKeyEqual("actions.0", "dont_notify")(result)
-				}),
-			),
-		)
+		for _, client := range []*client.CSAPI{alice, alice2} {
+			pushRulesAfter := client.GetAllPushRules(t)
+			must.MatchGJSON(t, pushRulesAfter,
+				match.JSONCheckOff("global.room", []interface{}{
+					roomID,
+					newRoomID,
+				},
+					match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
+					match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
+						return match.JSONKeyEqual("actions.0", "dont_notify")(result)
+					}),
+				),
+			)
+		}
 	})
 
 	// When a homeserver becomes aware of a room upgrade (upgrade is done on remote
-	// homeserver), it should copy over any push rules for all of its local users from the
-	// old room to the new room.
+	// homeserver), it should copy over any existing push rules for all of its local users
+	// from the old room to the new room at the time of upgrade.
 	t.Run("joining a remote upgraded room carries over the push rules", func(t *testing.T) {
 		// Alice create a room
 		roomID := alice.MustCreateRoom(t, map[string]interface{}{
@@ -81,42 +99,58 @@ func TestPushRuleRoomUpgrade(t *testing.T) {
 		bob.MustJoinRoom(t, roomID, []spec.ServerName{
 			deployment.GetFullyQualifiedHomeserverName(t, "hs1"),
 		})
+		// Remote bob2 joins the room
+		//
+		// We use two users to ensure that all users get taken care of (not just the first
+		// user on the homeserver).
+		bob2.MustJoinRoom(t, roomID, []spec.ServerName{
+			deployment.GetFullyQualifiedHomeserverName(t, "hs1"),
+		})
 
-		// Bob adds some push rules
+		// Add some push rules
 		bob.MustDo(t, "PUT", []string{"_matrix", "client", "v3", "pushrules", "global", "room", roomID},
+			client.WithJSONBody(t, map[string]interface{}{
+				"actions": []string{"dont_notify"},
+			}),
+		)
+		bob2.MustDo(t, "PUT", []string{"_matrix", "client", "v3", "pushrules", "global", "room", roomID},
 			client.WithJSONBody(t, map[string]interface{}{
 				"actions": []string{"dont_notify"},
 			}),
 		)
 
 		// Sanity check the push rules are in the expected state before the upgrade
-		pushRulesBefore := bob.GetAllPushRules(t)
-		must.MatchGJSON(t, pushRulesBefore,
-			match.JSONCheckOff("global.room", []interface{}{
-				roomID,
-			},
-				match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
-				match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
-					return match.JSONKeyEqual("actions.0", "dont_notify")(result)
-				}),
-			),
-		)
+		for _, client := range []*client.CSAPI{bob, bob2} {
+			pushRulesBefore := client.GetAllPushRules(t)
+			must.MatchGJSON(t, pushRulesBefore,
+				match.JSONCheckOff("global.room", []interface{}{
+					roomID,
+				},
+					match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
+					match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
+						return match.JSONKeyEqual("actions.0", "dont_notify")(result)
+					}),
+				),
+			)
+		}
 
 		// Upgrade the room
 		newRoomID := alice.MustUpgradeRoom(t, roomID, "11")
 
 		// Sanity check the push rules are in the expected state before the upgrade
-		pushRulesAfter := bob.GetAllPushRules(t)
-		must.MatchGJSON(t, pushRulesAfter,
-			match.JSONCheckOff("global.room", []interface{}{
-				roomID,
-				newRoomID,
-			},
-				match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
-				match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
-					return match.JSONKeyEqual("actions.0", "dont_notify")(result)
-				}),
-			),
-		)
+		for _, client := range []*client.CSAPI{bob, bob2} {
+			pushRulesAfter := client.GetAllPushRules(t)
+			must.MatchGJSON(t, pushRulesAfter,
+				match.JSONCheckOff("global.room", []interface{}{
+					roomID,
+					newRoomID,
+				},
+					match.CheckOffMapper(func(r gjson.Result) interface{} { return r.Get("rule_id").Str }),
+					match.CheckOffForEach(func(roomIDFromPushRule interface{}, result gjson.Result) error {
+						return match.JSONKeyEqual("actions.0", "dont_notify")(result)
+					}),
+				),
+			)
+		}
 	})
 }
