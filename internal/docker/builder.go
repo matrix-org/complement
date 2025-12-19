matrix-org/complement

commit 38dfe7bb95d3e5ac57c937d4be4751af5f71bca8

Use `host-gateway` to ensure servers can reach host (#389)
* Note that Docker >= 20.10 is required.

Co-authored-by: reivilibre <olivier@librepush.net>

internal/docker/deployer.go
@@ -187,10 +187,11 @@ func deployImage(
 	var err error
 
 	if runtime.GOOS == "linux" {
-		// By default docker for linux does not expose this, so do it now.
-		// When https://github.com/moby/moby/pull/40007 lands in Docker 20, we should
-		// change this to be  `host.docker.internal:host-gateway`
-		extraHosts = []string{HostnameRunningComplement + ":172.17.0.1"}
+		// Ensure that the homeservers under test can contact the host, so they can
+		// interact with a complement-controlled test server.
+		// Note: this feature of docker landed in Docker 20.10,
+		// see https://github.com/moby/moby/pull/40007 
+		extraHosts = []string{"host.docker.internal:host-gateway"}
 	}
 
 	for _, m := range cfg.HostMounts {

commit f86dadbbb8217db59e58b2027298f387af0dc1f6

Use `COMPLEMENT_HOSTNAME_RUNNING_COMPLEMENT` config for `ExtraHosts` in Docker (#825)
(doesn't fix anything specifically, just aligning to what we should be doing)

It looks like this regressed in https://github.com/matrix-org/complement/pull/389 (previously, we we're using `HostnameRunningComplement` here)

internal/docker/builder.go
@@ -32,13 +32,6 @@ import (
 	"github.com/matrix-org/complement/internal/instruction"
 )
 
-var (
-	// HostnameRunningDocker is the hostname of the docker daemon from the perspective of Complement.
-	HostnameRunningDocker = "localhost"
-	// HostnameRunningComplement is the hostname of Complement from the perspective of a Homeserver.
-	HostnameRunningComplement = "host.docker.internal"
-)
-
 const complementLabel = "complement_context"
 
 type Builder struct {

internal/docker/deployer.go
@@ -348,7 +348,7 @@ func deployImage(
 		// interact with a complement-controlled test server.
 		// Note: this feature of docker landed in Docker 20.10,
 		// see https://github.com/moby/moby/pull/40007
-		extraHosts = []string{"host.docker.internal:host-gateway"}
+		extraHosts = []string{fmt.Sprintf("%s:host-gateway", cfg.HostnameRunningComplement)}
 	}
 
 	for _, m := range cfg.HostMounts {