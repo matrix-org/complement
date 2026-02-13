7a77aa759eb68937212d51f092a833c0d09fe53b
Eric Eastwood <erice@element.io>
2026-02-13 12:34:31 -0600

    Deduplicate assertion logic (`testInitialSyncStateAfterIncludesTimelineSenders`)

--- tests/msc4222/msc4222_test.go
+++ tests/msc4222/msc4222_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/matrix-org/complement"
 	"github.com/matrix-org/complement/client"
 	"github.com/matrix-org/complement/helpers"
-	"github.com/matrix-org/complement/must"
 	"github.com/matrix-org/complement/should"
 	"github.com/tidwall/gjson"
 )
@@ -39,67 +38,15 @@ func TestSync(t *testing.T) {
 				// Make double sure that bob is joined to the room
 				alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(bob.UserID, roomID))
 
-				// Now, Alice makes an initial sync request with lazy-loading members enabled
-				//
-				// The spec says `lazy_load_members` is valid field for both `timeline` and
-				// `state` but as far as I can tell, only makes sense for `state` and that's
-				// what Synapse keys off of.
-				aliceSyncFilter := `{
-				    "room": {
-					    "timeline": { "limit": 20 },
-					    "state": { "lazy_load_members": true }
+				// Ensure `state_after` looks correct
+				expectedSendersFromTimeline := []string{ alice.UserID, bob.UserID }
+				syncFilter := `{
+					"room": {
+						"timeline": { "limit": 20 },
+						"state": { "lazy_load_members": true }
 					}
 				}`
-				res, _ := alice.MustSync(t, client.SyncReq{UseStateAfter: true, Filter: aliceSyncFilter})
-				joinedRoomRes := res.Get("rooms.join." + client.GjsonEscape(roomID))
-				if !joinedRoomRes.Exists() {
-					t.Fatalf("Unable to find roomID=%s in the join part of the sync response: %s", roomID, res)
-				}
-
-				// Collect the senders of all the time timeline events.
-				roomTimelineRes := joinedRoomRes.Get("timeline.events");
-				if !roomTimelineRes.IsArray() {
-					t.Fatalf("Timeline events is not an array (found %s) %s", roomTimelineRes.Type.String(), res)
-				}
-				sendersFromTimeline := make(map[string]struct{}, 0)
-				for _, event := range roomTimelineRes.Array() {
-					sendersFromTimeline[event.Get("sender").Str] = struct{}{}
-				}
-				// We expect to see timeline events from alice and bob
-				must.ContainSubset(t,
-					slices.Collect(maps.Keys(sendersFromTimeline)),
-					[]string{ alice.UserID, bob.UserID },
-				)
-
-				// Collect the `m.room.membership` from `state_after`
-				//
-				// Try looking up the stable variant `state_after` first, then fallback to the
-				// unstable version
-				roomStateAfterResStable := joinedRoomRes.Get("state_after.events");
-				roomStateAfterResUnstable := joinedRoomRes.Get("org\\.matrix\\.msc4222\\.state_after.events");
-				var roomStateAfterRes gjson.Result
-				if roomStateAfterResStable.Exists() {
-					roomStateAfterRes = roomStateAfterResStable
-				} else if roomStateAfterResUnstable.Exists() {
-					roomStateAfterRes = roomStateAfterResUnstable
-				}
-				// Sanity check syntax
-				if !roomStateAfterRes.IsArray() {
-					t.Fatalf("state_after events is not an array (found %s) %s", roomStateAfterRes.Type.String(), res)
-				}
-				membershipFromState := make(map[string]struct{}, 0)
-				for _, event := range roomStateAfterRes.Array() {
-					if event.Get("type").Str == "m.room.member" {
-						membershipFromState[event.Get("sender").Str] = struct{}{}
-					}
-				}
-				// We should see membership state from every `sender` in the `timeline` (alice
-				// and bob).
-				must.ContainSubset(t,
-					slices.Collect(maps.Keys(membershipFromState)),
-					slices.Collect(maps.Keys(sendersFromTimeline)),
-				)
-
+				testInitialSyncStateAfterIncludesTimelineSenders(t, alice, roomID, expectedSendersFromTimeline, syncFilter)
 		})
 
 		// When lazy-loading room members is enabled, for a private room, the `state_after`
@@ -127,87 +74,102 @@ func TestSync(t *testing.T) {
 				// Make double sure that bob is joined to the room
 				alice.MustSyncUntil(t, client.SyncReq{}, client.SyncJoinedTo(bob.UserID, roomID))
 
-				// Now, Alice makes an initial sync request with lazy-loading members enabled
-				//
-				// The spec says `lazy_load_members` is valid field for both `timeline` and
-				// `state` but as far as I can tell, only makes sense for `state` and that's
-				// what Synapse keys off of.
-				aliceSyncFilter := `{
-				    "room": {
-					    "timeline": { "limit": 20 },
-					    "state": { "lazy_load_members": true }
+				// Ensure `state_after` looks correct
+				expectedSendersFromTimeline := []string{ alice.UserID, bob.UserID }
+				syncFilter := `{
+					"room": {
+						"timeline": { "limit": 20 },
+						"state": { "lazy_load_members": true }
 					}
 				}`
-				res, _ := alice.MustSync(t, client.SyncReq{UseStateAfter: true, Filter: aliceSyncFilter})
-				joinedRoomRes := res.Get("rooms.join." + client.GjsonEscape(roomID))
-				if !joinedRoomRes.Exists() {
-					t.Fatalf("Unable to find roomID=%s in the join part of the sync response: %s", roomID, res)
-				}
-
-				// Collect the senders of all the time timeline events.
-				roomTimelineRes := joinedRoomRes.Get("timeline.events");
-				if !roomTimelineRes.IsArray() {
-					t.Fatalf("Timeline events is not an array (found %s) %s", roomTimelineRes.Type.String(), res)
-				}
-				sendersFromTimeline := make(map[string]struct{}, 0)
-				for _, event := range roomTimelineRes.Array() {
-					sendersFromTimeline[event.Get("sender").Str] = struct{}{}
-				}
-				// We expect to see timeline events from alice and bob
-				expectedSendersFromTimeline := []string{ alice.UserID, bob.UserID }
-				err := should.ContainSubset(
-					slices.Collect(maps.Keys(sendersFromTimeline)),
-					expectedSendersFromTimeline,
-				)
-				if err != nil {
-					t.Fatalf(
-						"Expected to see timeline events from (%s) but only saw %s. " +
-						"Got error: %s. join part of the sync response: %s",
-						expectedSendersFromTimeline,
-						slices.Collect(maps.Keys(sendersFromTimeline)),
-						err.Error(),
-						res,
-					)
-				}
-
-				// Collect the `m.room.membership` from `state_after`
-				//
-				// Try looking up the stable variant `state_after` first, then fallback to the
-				// unstable version
-				roomStateAfterResStable := joinedRoomRes.Get("state_after.events");
-				roomStateAfterResUnstable := joinedRoomRes.Get("org\\.matrix\\.msc4222\\.state_after.events");
-				var roomStateAfterRes gjson.Result
-				if roomStateAfterResStable.Exists() {
-					roomStateAfterRes = roomStateAfterResStable
-				} else if roomStateAfterResUnstable.Exists() {
-					roomStateAfterRes = roomStateAfterResUnstable
-				}
-				// Sanity check syntax
-				if !roomStateAfterRes.IsArray() {
-					t.Fatalf("state_after events is not an array (found %s) %s", roomStateAfterRes.Type.String(), res)
-				}
-				membershipFromState := make(map[string]struct{}, 0)
-				for _, event := range roomStateAfterRes.Array() {
-					if event.Get("type").Str == "m.room.member" {
-						membershipFromState[event.Get("sender").Str] = struct{}{}
-					}
-				}
-				// We should see membership state from every `sender` in the `timeline` (alice
-				// and bob).
-				err = should.ContainSubset(
-					slices.Collect(maps.Keys(membershipFromState)),
-					slices.Collect(maps.Keys(sendersFromTimeline)),
-				)
-				if err != nil {
-					t.Fatalf(
-						"Expected to see membership state (%s) from every sender in the timeline (%s). " +
-						"Got error: %s. join part of the sync response: %s",
-						slices.Collect(maps.Keys(membershipFromState)),
-						slices.Collect(maps.Keys(sendersFromTimeline)),
-						err.Error(),
-						res,
-					)
-				}
+				testInitialSyncStateAfterIncludesTimelineSenders(t, alice, roomID, expectedSendersFromTimeline, syncFilter)
 		})
 	})
+}
+
+
+// The `state_after` in an initial sync request should at-least include membership from
+// every `sender` in the `timeline`.
+func testInitialSyncStateAfterIncludesTimelineSenders(
+	t *testing.T,
+	syncingUser *client.CSAPI,
+	roomID string,
+	expectedSendersFromTimeline []string,
+	syncFilter string,
+) {
+	t.Helper()
+
+	// `syncingUser` makes an initial sync request with lazy-loading members enabled
+	//
+	// The spec says `lazy_load_members` is valid field for both `timeline` and
+	// `state` but as far as I can tell, only makes sense for `state` and that's
+	// what Synapse keys off of.
+	res, _ := syncingUser.MustSync(t, client.SyncReq{UseStateAfter: true, Filter: syncFilter})
+	joinedRoomRes := res.Get("rooms.join." + client.GjsonEscape(roomID))
+	if !joinedRoomRes.Exists() {
+		t.Fatalf("Unable to find roomID=%s in the join part of the sync response: %s", roomID, res)
+	}
+
+	// Collect the senders of all the time timeline events.
+	roomTimelineRes := joinedRoomRes.Get("timeline.events");
+	if !roomTimelineRes.IsArray() {
+		t.Fatalf("Timeline events is not an array (found %s) %s", roomTimelineRes.Type.String(), res)
+	}
+	sendersFromTimeline := make(map[string]struct{}, 0)
+	for _, event := range roomTimelineRes.Array() {
+		sendersFromTimeline[event.Get("sender").Str] = struct{}{}
+	}
+	// We expect to see timeline events from `expectedSendersFromTimeline`
+	err := should.ContainSubset(
+		slices.Collect(maps.Keys(sendersFromTimeline)),
+		expectedSendersFromTimeline,
+	)
+	if err != nil {
+		t.Fatalf(
+			"Expected to see timeline events from (%s) but only saw %s. " +
+			"Got error: %s. join part of the sync response: %s",
+			expectedSendersFromTimeline,
+			slices.Collect(maps.Keys(sendersFromTimeline)),
+			err.Error(),
+			res,
+		)
+	}
+
+	// Collect the `m.room.membership` from `state_after`
+	//
+	// Try looking up the stable variant `state_after` first, then fallback to the
+	// unstable version
+	roomStateAfterResStable := joinedRoomRes.Get("state_after.events");
+	roomStateAfterResUnstable := joinedRoomRes.Get("org\\.matrix\\.msc4222\\.state_after.events");
+	var roomStateAfterRes gjson.Result
+	if roomStateAfterResStable.Exists() {
+		roomStateAfterRes = roomStateAfterResStable
+	} else if roomStateAfterResUnstable.Exists() {
+		roomStateAfterRes = roomStateAfterResUnstable
+	}
+	// Sanity check syntax
+	if !roomStateAfterRes.IsArray() {
+		t.Fatalf("state_after events is not an array (found %s) %s", roomStateAfterRes.Type.String(), res)
+	}
+	membershipFromState := make(map[string]struct{}, 0)
+	for _, event := range roomStateAfterRes.Array() {
+		if event.Get("type").Str == "m.room.member" {
+			membershipFromState[event.Get("sender").Str] = struct{}{}
+		}
+	}
+	// We should see membership state from every `sender` in the `timeline`.
+	err = should.ContainSubset(
+		slices.Collect(maps.Keys(membershipFromState)),
+		slices.Collect(maps.Keys(sendersFromTimeline)),
+	)
+	if err != nil {
+		t.Fatalf(
+			"Expected to see membership state (%s) from every sender in the timeline (%s). " +
+			"Got error: %s. join part of the sync response: %s",
+			slices.Collect(maps.Keys(membershipFromState)),
+			slices.Collect(maps.Keys(sendersFromTimeline)),
+			err.Error(),
+			res,
+		)
+	}
 }
