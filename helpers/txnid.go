5de6121a8057828c4a4bb8ef71c178d3bc35e03c
Tulir Asokan <tulir@maunium.net>
2026-03-18 17:33:45 +0200

    Add doc for GetTxnID

--- helpers/txnid.go
+++ helpers/txnid.go
@@ -9,6 +9,8 @@ import (
 var txnCounter atomic.Int64
 var start = time.Now().Unix()
 
+// GetTxnID generates a unique transaction ID for use with endpoints like /send/{eventType/{txnId}.
+// The prefix is only for debugging purposes.
 func GetTxnID(prefix string) (txnID string) {
 	return fmt.Sprintf("%s-%d-%d", prefix, start, txnCounter.Add(1))
 }
