ea5943f3d37a1439a3fb9c77edd157e7a4d722a3
Eric Eastwood <erice@element.io>
2026-07-13 17:35:03 -0500

    Update docstring to indicate it no longer needs to be destroyed

--- tests/msc3902/federation_room_join_partial_state_test.go
+++ tests/msc3902/federation_room_join_partial_state_test.go
@@ -4265,7 +4265,7 @@ type partialStateJoinResult struct {
 
 // beginPartialStateJoin has a test user attempt to join the given room.
 //
-// It returns a partialStateJoinResult, which must be Destroy'd on completion.
+// # It returns a partialStateJoinResult
 //
 // When this method completes, the /join request will have completed, but the
 // state has not yet been re-synced. To allow the re-sync to proceed, call
